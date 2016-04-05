03a9102ca90d597b614102705b3c9da9e457cbc5
Karl Hardenstine <khardenstine@gmail.com>
2016-04-05 13:21:10 -0400

    Trigger reagent render on promise resolve

--- src/hypercrud_client/core.cljs
+++ src/hypercrud_client/core.cljs
@@ -12,14 +12,14 @@
 (def content-type-transit "application/transit+json;charset=UTF-8")
 
 (def transit-encoding-opts {:handlers {goog.Uri (util/UriHandler.)}})
-(def transit-decoding-opts {"r" (fn [v] (goog.Uri. v))})
+(def transit-decoding-opts {:handlers {"r" (fn [v] (goog.Uri. v))}})
 
 
 (defmethod kvlt.middleware.params/coerce-form-params (keyword content-type-transit) [{:keys [form-params]}]
   (util/transit-encode form-params :json-verbose transit-encoding-opts))
 
 
-(defmethod kvlt.middleware/from-content-type (keyword content-type-transit)  [resp]
+(defmethod kvlt.middleware/from-content-type (keyword content-type-transit) [resp]
   (let [decoded-val (util/transit-decode (:body resp) :json-verbose transit-decoding-opts)]
     (assoc resp :body decoded-val)))
 
@@ -54,33 +54,35 @@
         (if loading-comp (loading-comp) [:div "loading"])))))
 
 
-(deftype HypercrudClient [^goog.Uri entry-uri cur]
+(deftype HypercrudClient [^goog.Uri entry-uri state]
   Hypercrud
   (create [this ^goog.Uri relative-href form]
     (assert (not (nil? relative-href)))
     (kvlt/request!
-      {:url (resolve-root-relative-uri entry-uri relative-href)
-       :headers {"content-type" content-type-transit
-                 "accept" content-type-transit}
-       :form form}))
+      {:url          (resolve-root-relative-uri entry-uri relative-href)
+       :content-type content-type-transit
+       :accept       content-type-transit
+       :method       :post
+       :form         form}))
 
   (read [this ^goog.Uri relative-href]
     (assert (not (nil? relative-href)))
     (kvlt/request!
-      {:url    (resolve-root-relative-uri entry-uri relative-href)
-       :headers {:accept content-type-transit}
-       :method :get
-       :as     :auto}))
+      {:url     (resolve-root-relative-uri entry-uri relative-href)
+       :accept  content-type-transit
+       :method  :get
+       :as      :auto}))
 
   (update [this ^goog.Uri relative-href form]
     (assert (not (nil? relative-href)))
-    (println (pr-str form))
-    (->> (kvlt/request! {:url (resolve-root-relative-uri entry-uri relative-href)
-                         :headers {"content-type" content-type-transit
-                                   "accept"       content-type-transit}
-                         :form form})
+    (js/alert (pr-str form))
+    (->> (kvlt/request! {:url          (resolve-root-relative-uri entry-uri relative-href)
+                         :content-type content-type-transit
+                         :accept       content-type-transit
+                         :method       :put
+                         :form         form})
          (fmap (fn [resp]
-                 (reset! (cur [:t]) (-> resp :body :t))
+                 (swap! state update-in [:t] (constantly (-> resp :body :t)))
                  resp))))
 
   (delete [this ^goog.Uri relative-href]
@@ -88,7 +90,7 @@
     nil)
 
   (t [this]
-    @(cur [:t] 0))
+    (get-in @state [:t] 0))
 
   (loaded? [this {:keys [data] :as hc-node}]
     (not (nil? data)))
@@ -96,18 +98,18 @@
   (resolve* [this {:keys [href] :as cj-item}]
     (assert (not (nil? cj-item)) "resolve*: cj-item is nil")
     (assert (not (nil? href)) "resolve*: cj-item :href is nil")
-    (let [cache @(cur [:cache] {})
+    (let [cache (get-in @state [:cache] {})
           cached? (contains? cache href)]
       (if cached?
         (p/resolved (get cache href))
         (->> (read this href)
              (fmap (fn [resolved-cj-item-response]
-                    (let [resolved-cj-item (-> resolved-cj-item-response :body :hypercrud)
-                          cache (-> resolved-cj-item-response :body :cache)
-                          cache (assoc cache href resolved-cj-item)]
-                      (assert resolved-cj-item (str "bad href: " href))
-                      (swap! (cur [:cache]) merge cache)
-                      resolved-cj-item)))))))
+                     (let [resolved-cj-item (-> resolved-cj-item-response :body :hypercrud)
+                           cache (-> resolved-cj-item-response :body :cache)
+                           cache (assoc cache href resolved-cj-item)]
+                       (assert resolved-cj-item (str "bad href: " href))
+                       (swap! state update-in [:cache] merge cache)
+                       resolved-cj-item)))))))
 
   (enter [this comp]
     (resolve this {:href entry-uri} comp))
