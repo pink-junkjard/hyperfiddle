2faaba28abf29541a04d838874cbcc8a33b0ee1f
Dustin Getz <dustin.getz@gmail.com>
2016-04-29 10:36:24 -0400

    Project cleanup

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to hypercrud-client
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/hypercrud_client/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns hypercrud-client.core-test
-  (:require [clojure.test :refer :all]
-            [hypercrud-client.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
