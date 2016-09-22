407086d997f3ac4792fc219cdc1f72c68765754e
Karl Hardenstine <khardenstine@gmail.com>
2016-09-22 17:15:59 -0400

    Pass props through to input field

--- src/hypercrud/ui/input.cljs
+++ src/hypercrud/ui/input.cljs
@@ -3,16 +3,16 @@
 
 
 ; TODO bug - if value controlled from above, this will ignore it
-(defn validated-input [value on-change! parse-string to-string valid? & props]
+(defn validated-input [value on-change! parse-string to-string valid? & [props]]
   (let [intermediate-val (reagent/atom (to-string value))]
     (fn [value on-change! parse-string to-string valid?]
-      [:input {:type "text"
-               :class (if-not (valid? @intermediate-val) "invalid")
-               :value @intermediate-val
-               :on-change #(reset! intermediate-val (.. % -target -value))
-               :on-blur #(if (valid? @intermediate-val)
-                          (on-change! (parse-string @intermediate-val)))}])))
+      [:input (merge props {:type "text"
+                            :class (if-not (valid? @intermediate-val) "invalid")
+                            :value @intermediate-val
+                            :on-change #(reset! intermediate-val (.. % -target -value))
+                            :on-blur #(if (valid? @intermediate-val)
+                                       (on-change! (parse-string @intermediate-val)))})])))
 
 
-(defn input* [value on-change! & props]
+(defn input* [value on-change! & [props]]
   [validated-input value on-change! identity identity (constantly true) props])
