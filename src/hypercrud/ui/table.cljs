6c7424df8734c370fe8cb9b90a1882ee78fb2651
Karl Hardenstine <khardenstine@gmail.com>
2016-10-05 15:38:57 -0400

    fix dangling row in table query results create new

--- src/hypercrud/ui/table.cljs
+++ src/hypercrud/ui/table.cljs
@@ -21,53 +21,59 @@
        (get forms form-id)))
 
 
-(defn table [graph eids forms queries form-id expanded-cur stage-tx! navigate-cmp retract-entity]
+(defn body [graph eids forms queries form-id expanded-cur stage-tx! navigate-cmp retract-entity]
   (let [new-entities (r/atom [])]
     (fn [graph eids forms queries form-id expanded-cur stage-tx! navigate-cmp retract-entity]
       (let [form (get forms form-id)]
-        [:table.ui-table
-         [:colgroup [:col {:span "1" :style {:width "20px"}}]]
-         [:thead
-          [:tr
-           (if retract-entity [:td.remove-row {:key "remove-col"}])
-           [:td {:key "select-col"}]
-           (build-col-heads form)]]
-         [:tbody
-          (concat
-            (let [entities (map #(hc/entity graph %) eids)
-                  ; hack todo we need sortkeys in our form construct
-                  sortkey (->> (map :ident form)
-                               (filter #(-> % name (= "name")))
-                               (first))
-                  entities (concat (if sortkey (sort-by sortkey entities) entities)
-                                   (map #(hc/entity graph %) @new-entities))]
-              (map (fn [{:keys [:db/id] :as entity}]
-                     [:tr {:key id}
-                      (if retract-entity
-                        [:td.remove-row {:key "remove"}
-                         [:button {:on-click #(retract-entity id)} "⌦"]])
-                      [:td.id {:key "edit-td"}
-                       [navigate-cmp {:href (str form-id "/entity/" id)} (if (neg? id) id (mod id 100))]]
-                      (build-row-cells form-id entity {:expanded-cur (expanded-cur [id])
-                                                       :forms forms
-                                                       :graph graph
-                                                       :navigate-cmp navigate-cmp
-                                                       :queries queries
-                                                       :stage-tx! stage-tx!})])
-                   entities))
-            [(let [id (hc/*temp-id!*)]
-               [:tr {:key (hash @new-entities)}
-                (if retract-entity
-                  [:td.remove-row {:key "remove"}])
-                [:td.id {:key "edit-td"}]
-                (build-row-cells form-id {:db/id id} {:expanded-cur (expanded-cur [id])
+        [:tbody
+         (concat
+           (let [entities (map #(hc/entity graph %) eids)
+                 ; hack todo we need sortkeys in our form construct
+                 sortkey (->> (map :ident form)
+                              (filter #(-> % name (= "name")))
+                              (first))
+                 entities (concat (if sortkey (sort-by sortkey entities) entities)
+                                  (map #(hc/entity graph %) @new-entities))]
+             (map (fn [{:keys [:db/id] :as entity}]
+                    [:tr {:key id}
+                     (if retract-entity
+                       [:td.remove-row {:key "remove"}
+                        [:button {:on-click #(retract-entity id)} "⌦"]])
+                     [:td.id {:key "edit-td"}
+                      [navigate-cmp {:href (str form-id "/entity/" id)} (if (neg? id) id (mod id 100))]]
+                     (build-row-cells form-id entity {:expanded-cur (expanded-cur [id])
                                                       :forms forms
                                                       :graph graph
                                                       :navigate-cmp navigate-cmp
                                                       :queries queries
-                                                      :stage-tx! (fn [tx]
-                                                                   (swap! new-entities conj id)
-                                                                   (stage-tx! tx))})])])]]))))
+                                                      :stage-tx! stage-tx!})])
+                  entities))
+           [(let [id (hc/*temp-id!*)]
+              [:tr {:key (hash @new-entities)}
+               (if retract-entity
+                 [:td.remove-row {:key "remove"}])
+               [:td.id {:key "edit-td"}]
+               (build-row-cells form-id {:db/id id} {:expanded-cur (expanded-cur [id])
+                                                     :forms forms
+                                                     :graph graph
+                                                     :navigate-cmp navigate-cmp
+                                                     :queries queries
+                                                     :stage-tx! (fn [tx]
+                                                                  (swap! new-entities conj id)
+                                                                  (stage-tx! tx))})])])]))))
+
+
+(defn table [graph eids forms queries form-id expanded-cur stage-tx! navigate-cmp retract-entity]
+  (let [form (get forms form-id)]
+    [:table.ui-table
+     [:colgroup [:col {:span "1" :style {:width "20px"}}]]
+     [:thead
+      [:tr
+       (if retract-entity [:td.remove-row {:key "remove-col"}])
+       [:td {:key "select-col"}]
+       (build-col-heads form)]]
+     ^{:key (hc/t graph)}
+     [body graph eids forms queries form-id expanded-cur stage-tx! navigate-cmp retract-entity]]))
 
 
 (defn table-pull-exp [form]
