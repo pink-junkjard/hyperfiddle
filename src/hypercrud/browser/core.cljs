0715f0d603d627af82e46d74dfd1cc7ccc0203e0
Karl Hardenstine <khardenstine@gmail.com>
2016-11-04 12:55:48 -0400

    Drop unneeded features from browser

--- src/hypercrud/browser/core.cljs
+++ src/hypercrud/browser/core.cljs
@@ -1,22 +1,16 @@
 (ns hypercrud.browser.core
-  (:require [cljs.pprint :as pprint]
-            [cljs.reader :as reader]
+  (:require [cljs.reader :as reader]
             [clojure.string :as string]
             [hypercrud.browser.base-64-url-safe :as base64]
-            [hypercrud.browser.pages.dump :as dump]
             [hypercrud.browser.pages.entity :as entity]
-            [hypercrud.browser.pages.export-datoms :as export-datoms]
             [hypercrud.browser.pages.field :as field]
-            [hypercrud.browser.pages.hydrate :as hydrate]
-            [hypercrud.browser.pages.transact :as transact]
-            [hypercrud.browser.pages.index :as index]
             [hypercrud.browser.pages.query :as query]
             [hypercrud.client.core :as hc]
             [hypercrud.client.internal :as internal]
             [hypercrud.types :refer [->DbId ->DbVal]]))
 
 
-(defn route [page-rel-path {:keys [query-fn entity-fn field-fn index-fn dump-fn export-fn hydrate-fn transact-fn else]}]
+(defn route [page-rel-path {:keys [query-fn entity-fn field-fn else]}]
   (let [path-params (string/split page-rel-path "/")]
     (cond
       (and (= (second path-params) "query") (= 4 (count path-params)))
@@ -44,28 +38,10 @@
         ;todo this should accept a real entity type
         (field-fn dbval dbid field-id))
 
-      (and (= (first path-params) "dump") (= 3 (count path-params)))
-      (let [[_ id conn-id] path-params
-            id (js/parseInt (second path-params) 10)
-            dbid (->DbId id conn-id)
-            dbval (->DbVal conn-id nil)]
-        (dump-fn dbid dbval))
-
-      (and (= (first path-params) "export") (= 1 (count path-params)))
-      (export-fn)
-
-      (and (= (first path-params) "hydrate") (= 1 (count path-params)))
-      (hydrate-fn)
-
-      #_(and (= (first path-params) "transact") (= 1 (count path-params)))
-      #_(transact-fn)
-
-      (and (= (first path-params) "") (= 1 (count path-params)))
-      (index-fn)
       :else (else))))
 
 
-(defn ui [cur editor-graph links stage-tx! graph page-rel-path navigate! navigate-cmp param-ctx]
+(defn ui [cur editor-graph stage-tx! graph page-rel-path navigate! navigate-cmp param-ctx]
   [:div
    [:div.hc-node-view
     (route page-rel-path
@@ -81,19 +57,7 @@
                         (let [entity (hc/entity (hc/get-dbgraph graph dbval) dbid)
                               field (hc/entity editor-graph (->DbId field-id (-> editor-graph .-dbval .-conn-id)))]
                           (field/ui cur stage-tx! graph entity field navigate-cmp)))
-            :index-fn (fn []
-                        (let [links (map #(hc/entity editor-graph %) links)]
-                          (index/ui links navigate-cmp param-ctx)))
-            :dump-fn (fn [dbid dbval]
-                       (let [entity (hc/entity (hc/get-dbgraph graph dbval) dbid)]
-                         (dump/ui entity)))
-            :export-fn (fn [] (export-datoms/ui cur graph))
-            :hydrate-fn (fn [] (hydrate/ui cur graph))
-            ;:transact-fn (fn [] (transact/ui cur transact! graph))
-            :else (constantly [:div "no route for: " page-rel-path])})]
-   #_[:pre (pr-str param-ctx)]
-   #_[:hr]
-   #_[:pre (with-out-str (pprint/pprint @cur))]])
+            :else (constantly [:div "no route for: " page-rel-path])})]])
 
 
 (defn query [state editor-graph page-rel-path param-ctx]
@@ -108,9 +72,4 @@
           :field-fn (fn [dbval dbid field-id]
                       (let [field (hc/entity editor-graph (->DbId field-id (-> editor-graph .-dbval .-conn-id)))]
                         (field/query state dbid field param-ctx)))
-          :index-fn #(index/query)
-          :dump-fn (fn [dbid dbval] (dump/query dbval dbid))
-          :export-fn (fn [] (export-datoms/query state (.-schema editor-graph) param-ctx))
-          :hydrate-fn (fn [] (hydrate/query state))
-          ;:transact-fn (fn [] (transact/query))
           :else (constantly {})}))
