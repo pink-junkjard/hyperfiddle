45e03ef89519f1a6cd4a08ae39ed872bce25a5f9
Dustin Getz <dustin.getz@gmail.com>
2016-11-07 13:40:06 -0500

    Use dbholes

--- src/hypercrud/browser/pages/query.cljs
+++ src/hypercrud/browser/pages/query.cljs
@@ -18,12 +18,13 @@
 (defn initial-entity [q holes-by-name params]
   (let [schema (->> holes-by-name
                     (map (fn [[hole-name hole]]
-                           [hole-name {:db/cardinality (:attribute/cardinality hole)}]))
+                           [hole-name {:db/valueType (-> hole :attribute/valueType :db/ident)
+                                       :db/cardinality (-> hole :attribute/cardinality :db/ident)}]))
                     (into {}))
         hole-names (q-util/parse-holes q)
         dbid (->DbId -1 :query-hole-form)
         dbval (->DbVal :query-hole-form nil)
-        data (-> (zipmap hole-names (drop 1 params))
+        data (-> (zipmap hole-names params)
                  (assoc :db/id dbid))]
     (->Entity (hc-g/->DbGraph schema dbval nil {} {}) dbid data {})))
 
@@ -38,6 +39,7 @@
   ;todo these ids are scary
   (->> hole-names
        (map #(get holes-by-name %))
+       (remove nil?)
        (mapcat (fn [{:keys [:hole/name
                             :field/prompt :field/query :field/label-prop
                             :attribute/valueType :attribute/cardinality] :as hole}]
@@ -60,8 +62,10 @@
         holes-by-name (->> (:query/hole query)
                            (map (juxt :hole/name identity))
                            (into {}))
-        entity-cur (cur [:holes] (initial-entity q hole-names params)) ;todo this needs to be hc/with
+        entity-cur (cur [:holes] (initial-entity q holes-by-name params)) ;todo this needs to be hc/with
         entity @entity-cur
+        dbhole-values (q-util/build-dbhole-lookup query)
+        hole-lookup (merge (-> entity .-data (dissoc :db/id)) dbhole-values)
         dbval (get param-ctx :dbval)]
     [:div
      #_[:pre (pr-str params)]                               ;; params are same information as the filled holes in this form below
@@ -72,7 +76,7 @@
            holes-form (hc/entity editor-graph holes-form-dbid)]
        [form/form graph entity holes-form expanded-cur stage-tx! navigate-cmp])
      [:hr]
-     (if (show-results? hole-names (-> entity .-data (dissoc :db/id))) ;todo what if we have a user hole?
+     (if (show-results? hole-names hole-lookup)             ;todo what if we have a user hole?
        (let [entities (->> (hc/select graph ::table/query)
                            (map #(hc/entity (hc/get-dbgraph graph dbval) %)))]
          (if (and (:query/single-result-as-entity? query) (= 1 (count entities)))
@@ -105,26 +109,31 @@
 (defn query [state editor-graph query params table-form param-ctx]
   (let [q (reader/read-string (:query/value query))
         hole-names (q-util/parse-holes q)
+        holes-by-name (->> (:query/hole query)
+                           (map (juxt :hole/name identity))
+                           (into {}))
         ;; this is the new "param-ctx" - it is different - we already have the overridden
         ;; values, there is no need to eval the formulas in a ctx to get the values.
         param-values (-> (or (get state :holes)
-                             (initial-entity q hole-names params))
+                             (initial-entity q holes-by-name params))
                          .-data
-                         (dissoc :db/id))]
+                         (dissoc :db/id))
+        dbhole-values (q-util/build-dbhole-lookup query)
+        hole-lookup (merge param-values dbhole-values)]
     (merge
       ;no fields are isComponent true or expanded, so we don't need to pass in a forms map
-      (let [holes-by-name (->> (:query/hole query)
-                               (map (juxt :hole/name identity))
-                               (into {}))
-            holes-form-dbid (->DbId -1 (-> editor-graph .-dbval .-conn-id))
+      (let [holes-form-dbid (->DbId -1 (-> editor-graph .-dbval .-conn-id))
             tx (holes->field-tx editor-graph holes-form-dbid hole-names holes-by-name)
             editor-graph (hc/with' editor-graph tx)
             holes-form (hc/entity editor-graph holes-form-dbid)]
         (form-util/form-option-queries holes-form nil q-util/build-params-from-formula param-ctx))
 
-      (if (show-results? hole-names param-values)
+      (if (show-results? hole-names hole-lookup)
         (let [p-filler (fn [query formulas param-ctx]
-                         (q-util/build-params #(get param-values %) query param-ctx))]
+                         (q-util/build-params (fn [hole-name]
+                                                (let [v (get hole-lookup hole-name)]
+                                                  (if (instance? hypercrud.types.DbId v) (.-id v) v)))
+                                              query param-ctx))]
           ; hacks so when result set is 1 we can display the entity view
           #_(table/query p-filler param-ctx {:link/query (:db/id query)
                                              :link/form form-id})
--- src/hypercrud/form/q_util.cljs
+++ src/hypercrud/form/q_util.cljs
@@ -1,6 +1,7 @@
 (ns hypercrud.form.q-util
   (:require [cljs.reader :as reader]
-            [hypercrud.compile.eval :as eval]))
+            [hypercrud.compile.eval :as eval]
+            [hypercrud.types :refer [->DbVal]]))
 
 
 (defn parse-holes [q]
@@ -11,8 +12,16 @@
     (->> (partition-by f q)
          (filter #(= :in (first %)))
          first
-         (drop 2)
+         (drop 1)
          (map str))))
+
+
+(defn build-dbhole-lookup [query]
+  (->> (:query/dbhole query)
+       (map (fn [{:keys [:dbhole/name :dbhole/value]}]
+              ; transform project-id into conn-id
+              [name (->DbVal (-> value .-dbid .-id) nil)]))
+       (into {})))
 
 
 ; type p-filler = fn [query formulas param-ctx] => vec
@@ -20,30 +29,32 @@
 (defn build-params [fill-hole query param-ctx]
   (let [q (reader/read-string (:query/value query))]
     (->> (parse-holes q)
-         (map (fn [hole-name]
-                (let [value (fill-hole hole-name param-ctx)]
-                  ;; Are you at the root? Can't fill holes without a project client
-                  (assert (not= nil value) (str "Empty parameter for " hole-name " in " q))
-                  value)))
-         (concat [(get param-ctx :dbval)]))))
+         (mapv (fn [hole-name]
+                 (let [value (fill-hole hole-name param-ctx)]
+                   ;; Are you at the root? Can't fill holes without a project client
+                   (assert (not= nil value) (str "Empty parameter for " hole-name " in " q))
+                   value))))))
 
 
-(defn fill-hole-from-formula [formulas]
+(defn fill-hole-from-formula [query formulas]
   (let [hole-formulas (if (empty? formulas)
                         {}
                         (->> formulas
                              reader/read-string
                              (map (juxt first #(eval/uate (str "(identity " (second %) ")"))))
-                             (into {})))]
+                             (into {})))
+        dbhole-values (build-dbhole-lookup query)]
     (fn [hole-name param-ctx]
-      (let [{formula :value error :error} (get hole-formulas hole-name)]
-        (if error
-          ; first error, lose the rest of the errors
-          (throw error)
+      (if-let [v (get dbhole-values hole-name)]
+        v
+        (let [{formula :value error :error} (get hole-formulas hole-name)]
+          (if error
+            ; first error, lose the rest of the errors
+            (throw error)
 
-          ; can also throw, lose the rest
-          (formula param-ctx))))))
+            ; can also throw, lose the rest
+            (formula param-ctx)))))))
 
 
 (defn build-params-from-formula [query formulas param-ctx]
-  (build-params (fill-hole-from-formula formulas) query param-ctx))
+  (build-params (fill-hole-from-formula query formulas) query param-ctx))
