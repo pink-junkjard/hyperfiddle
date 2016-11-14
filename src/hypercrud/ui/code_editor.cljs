331833be8154bf3ae4140e2cbac5d989224f9fda
Dustin Getz <dustin.getz@gmail.com>
2016-11-14 18:14:26 -0500

    fix code-mirror lifecycle methods when two codemirrors on page at same time causing NPEs inside codemirror

--- src/hypercrud/ui/code_editor.cljs
+++ src/hypercrud/ui/code_editor.cljs
@@ -1,28 +1,33 @@
 (ns hypercrud.ui.code-editor
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [reagent.impl.component :as r-comp]))
 
 
-(defn did-mount! [change!]
-  (fn [this]
-    (let [div (.querySelector (reagent/dom-node this) "textarea")
-          ref (.fromTextArea js/CodeMirror div
-                             #js {:mode "clojure"
-                                  :lineNumbers true
-                                  :matchBrackets true
-                                  :autoCloseBrackets true
-                                  :viewportMargin js/Infinity})]
-      (aset this "codeMirrorRef" ref)
-      (.on ref "blur" #(change! (.getValue %))))))
-
-
-(defn code-editor* [value change!]
+(def code-editor*
   (reagent/create-class
-    {:render (fn [this]
-               [:div.value
-                [:textarea {:default-value value :auto-complete "off" :class "text"}]])
-     :component-did-mount (did-mount! change!)
-     :component-will-unmount (fn [this] (.toTextArea (aget this "codeMirrorRef")))
+    {:reagent-render
+     (fn [value change!]
+       [:div.value
+        [:textarea {:default-value value :auto-complete "off" :class "text"}]])
 
-     :component-did-update (fn [this props]
-                             (let [[_ value change!] (aget this "props" "argv")] ; wtf reagent
-                               (.setValue (aget this "codeMirrorRef") value)))}))
+     :component-did-mount
+     (fn [this]
+       (let [[_ value change!] (r-comp/get-argv this)       ;[value change!] (reagent/props this)
+             div (.querySelector (reagent/dom-node this) "textarea")
+             ref (.fromTextArea js/CodeMirror div
+                                #js {:mode "clojure"
+                                     :lineNumbers true
+                                     :matchBrackets true
+                                     :autoCloseBrackets true
+                                     :viewportMargin js/Infinity})]
+         (aset this "codeMirrorRef" ref)
+         (.on ref "blur" #(change! (.getValue %)))))
+
+
+     :component-will-unmount
+     (fn [this] (.toTextArea (aget this "codeMirrorRef")))
+
+     :component-did-update
+     (fn [this]
+       (let [[_ value change!] (r-comp/get-argv this)]      ;[value change!] (reagent/props this)
+         (.setValue (aget this "codeMirrorRef") value)))}))
