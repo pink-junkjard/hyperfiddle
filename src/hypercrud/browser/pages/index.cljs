b2e560d8bbfb5600b1d1f6ebb3cee8a5a4992168
Dustin Getz <dustin.getz@gmail.com>
2016-12-01 13:04:16 -0500

    Fix browser index classes

--- src/hypercrud/browser/pages/index.cljs
+++ src/hypercrud/browser/pages/index.cljs
@@ -3,14 +3,12 @@
 
 
 (defn ui [links navigate-cmp param-ctx]
-  [:div
-   ;form to populate holes
-   [:ul.links
-    (->> links                                              ;todo this should be a select
-         (sort-by :link/prompt)
-         (map (fn [{:keys [:db/id :link/prompt] :as link}]
-                [:li {:key id}
-                 [navigate-cmp (links/query-link #() link param-ctx) prompt]])))]])
+  [:ul
+   (->> links
+        (sort-by :link/prompt)
+        (map (fn [{:keys [:db/id :link/prompt] :as link}]
+               [:li {:key id}
+                [navigate-cmp (links/query-link #() link param-ctx) prompt]])))])
 
 
 ;todo we should query the links
