d90c8c77455d913a0ed1b3a2714d0bbe0cd651ac
Dustin Getz <dustin.getz@gmail.com>
2016-12-01 12:53:04 -0500

    memoize cljs eval - seattle 8s render -> 1s

--- src/hypercrud/compile/eval.cljs
+++ src/hypercrud/compile/eval.cljs
@@ -4,13 +4,14 @@
             [markdown.core]))
 
 
-(defn eval [code-str]
-  ;; Hack - we don't understand why cljs compiler doesn't handle top level forms naturally
-  ;; but wrapping in identity fixes the problem
-  (let [code-str' (str "(identity " code-str ")")]
-    (binding [analyzer/*cljs-warning-handlers* []]
-      (cljs/eval-str (cljs/empty-state)
-                     code-str'
-                     nil
-                     {:eval cljs/js-eval}
-                     identity))))
+(def eval
+  (memoize (fn [code-str]
+             ;; Hack - we don't understand why cljs compiler doesn't handle top level forms naturally
+             ;; but wrapping in identity fixes the problem
+             (let [code-str' (str "(identity " code-str ")")]
+               (binding [analyzer/*cljs-warning-handlers* []]
+                 (cljs/eval-str (cljs/empty-state)
+                                code-str'
+                                nil
+                                {:eval cljs/js-eval}
+                                identity))))))
