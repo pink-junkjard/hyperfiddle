98a309261fd5fd6568496ad440bfc75c5cd1e08d
Karl Hardenstine <khardenstine@gmail.com>
2016-12-03 02:08:28 -0500

    Remove :link/if-empty-result

--- src/hypercrud/browser/links.cljs
+++ src/hypercrud/browser/links.cljs
@@ -13,19 +13,10 @@
 
 
 (defn build-query-params [link param-ctx]
-  (let [empty-result-lookup (let [lookup (:link/if-empty-result link)]
-                              (if-not (empty? lookup)
-                                (reader/read-string lookup)))
-        query-params (q-util/build-params-map link param-ctx)
+  (let [query-params (q-util/build-params-map link param-ctx)
         tempid-hack (fn [{:keys [:find-element/connection :find-element/name]}]
-                      ;; because we don't yet send local-datoms up to datomic,
-                      ;; we may need to adjust the query results to account for local datoms.
-                      ;; This is not possible to do generally without DataScript but we special
-                      ;; case the create-new case because users need it
-                      (if-let [provided-val (get query-params (get empty-result-lookup name))]
-                        provided-val
-                        ; todo use the find-element connection's dbval
-                        (hc/*temp-id!* (.-conn-id (get param-ctx :dbval)))))]
+                      ; todo use the find-element connection's dbval
+                      (hc/*temp-id!* (.-conn-id (get param-ctx :dbval))))]
     {:query-params query-params
      ;; Create a result of shape [?e ?f] with new entities colored
      :create-new-find-elements (->> (:link/find-element link)
