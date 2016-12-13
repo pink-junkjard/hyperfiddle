9297437a49772a60825292fb3b6137fce191c2be
Karl Hardenstine <khardenstine@gmail.com>
2016-12-13 16:25:25 -0500

    Render inline forms in table cells

--- src/hypercrud/ui/table_cell.cljs
+++ src/hypercrud/ui/table_cell.cljs
@@ -14,36 +14,20 @@
 
 (defn ref-one-component [entity {:keys [field] :as widget-args}]
   [:div
-   (->> (get-in entity [(-> field :field/attribute :attribute/ident) :db/id])
+   #_(->> (get-in entity [(-> field :field/attribute :attribute/ident) :db/id])
         (pr-str))
    (widget/link-thing widget-args)
-   ;todo render inline links
-   ]
-  #_(let [ident (-> field :field/attribute :attribute/ident)
-          child-entity (get entity ident)]
-      [:div
-       (-> (option/label-prop field [child-entity])
-           str
-           ellipsis)]))
+   (widget/render-inline-links widget-args)])
 
 
 (defn ref-many [entity {:keys [field] :as widget-args}]
   [:div
-   (->> (get entity (-> field :field/attribute :attribute/ident))
+   #_(->> (get entity (-> field :field/attribute :attribute/ident))
         (mapv :db/id)
         (pr-str)
         (ellipsis 15))
    (widget/link-thing widget-args)
-   ;todo render inline links
-   ]
-  #_(let [ident (-> field :field/attribute :attribute/ident)]
-      [:div
-       (->> (get entity ident)
-            (map (fn [entity]
-                   (if (not= nil entity)
-                     (option/label-prop field [entity])
-                     "nil")))
-            (string/join ", "))]))
+   (widget/render-inline-links widget-args)])
 
 
 (defn other-many [entity {:keys [field navigate-cmp]}]
