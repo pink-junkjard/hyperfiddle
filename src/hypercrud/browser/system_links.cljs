30aef39276f3487d61b3c7d1ac485935b63bfa76
Dustin Getz <dustin.getz@gmail.com>
2017-02-09 15:48:27 -0500

    sys edit field link wip

--- src/hypercrud/browser/system_links.cljs
+++ src/hypercrud/browser/system_links.cljs
@@ -12,15 +12,29 @@
           (-> parent-link-dbid :conn-id)))
 
 
+(defn system-edit-field-link-dbid [find-element-id field-id parent-link-dbid]
+  (->DbId [(-> parent-link-dbid :id)
+           (-> parent-link-dbid :conn-id)
+           :system-edit-field
+           find-element-id
+           field-id]
+          (-> parent-link-dbid :conn-id)))
+
+
 (defn system-edit-link [find-element parent-link]
-  ; todo we can remove temp-id!
-  (let [new-link-entity-dbid (hc/*temp-id!* hc/*root-conn-id*)]
-    {:db/id (system-edit-link-dbid (-> find-element :db/id :id) (:db/id parent-link))
-     :link/name (str "edit " (:find-element/name find-element))
-     :hypercrud/owner (:hypercrud/owner parent-link)
-     :link/request {:db/id new-link-entity-dbid
-                    :link-entity/connection (:find-element/connection find-element)
-                    :link-entity/form (:find-element/form find-element)}}))
+  {:db/id (system-edit-link-dbid (-> find-element :db/id :id) (:db/id parent-link))
+   :link/name (str "edit " (:find-element/name find-element))
+   :hypercrud/owner (:hypercrud/owner parent-link)
+   :link/request {:link-entity/connection (:find-element/connection find-element)
+                  :link-entity/form (:find-element/form find-element)}})
+
+
+(defn system-edit-field-link [find-element field parent-link]
+  {:db/id (system-edit-field-link-dbid (-> find-element :db/id :id) (get-in field [:db/id :id]) (:db/id parent-link))
+   :link/name (str "edit " (:find-element/name find-element) " " (get-in field [:field/attribute :attribute/ident]))
+   :hypercrud/owner (:hypercrud/owner parent-link)
+   :link/request {:link-entity/connection (:find-element/connection find-element)
+                  :link-entity/form (:find-element/form find-element)}})
 
 
 (defn overlay-system-links-tx
@@ -32,21 +46,32 @@
                        (mapcat (fn [find-element]
                                  (let [edit-anchor-dbid (hc/*temp-id!* (-> parent-link :db/id :conn-id))
                                        create-anchor-dbid (hc/*temp-id!* (-> parent-link :db/id :conn-id))]
-                                   [{:db/id edit-anchor-dbid
-                                     :anchor/ident :edit
-                                     :anchor/prompt (str "edit " (:find-element/name find-element))
-                                     :anchor/link (system-edit-link find-element parent-link)
-                                     :anchor/repeating? true
-                                     :anchor/formula (pr-str {:entity-dbid-s (pr-str `(fn [~'ctx]
-                                                                                        (get-in ~'ctx [:result ~(:find-element/name find-element) :db/id])))})}
+                                   (concat
+                                     [{:db/id edit-anchor-dbid
+                                       :anchor/prompt (str "edit " (:find-element/name find-element))
+                                       :anchor/link (system-edit-link find-element parent-link)
+                                       :anchor/repeating? true
+                                       :anchor/find-element find-element
+                                       :anchor/formula (pr-str {:entity-dbid-s (pr-str `(fn [~'ctx]
+                                                                                          (get-in ~'ctx [:result ~(:find-element/name find-element) :db/id])))})}
 
-                                    {:db/id create-anchor-dbid
-                                     :anchor/ident :create
-                                     :anchor/prompt (str "create " (:find-element/name find-element))
-                                     :anchor/link (system-edit-link find-element parent-link)
-                                     :anchor/repeating? false
-                                     :anchor/formula (pr-str {:entity-dbid-s (pr-str `(fn [~'ctx]
-                                                                                        (hc/*temp-id!* ~(get-in find-element [:find-element/connection :db/id :id]))))})}])))
+                                      {:db/id create-anchor-dbid
+                                       :anchor/prompt (str "create " (:find-element/name find-element))
+                                       :anchor/link (system-edit-link find-element parent-link)
+                                       :anchor/repeating? false
+                                       :anchor/find-element find-element
+                                       :anchor/formula (pr-str {:entity-dbid-s (pr-str `(fn [~'ctx]
+                                                                                          (hc/*temp-id!* ~(get-in find-element [:find-element/connection :db/id :id]))))})}]
+                                 #_    (->> (-> find-element :find-element/form :form/field)
+                                          (filter #(= :db.type/ref (-> % :field/attribute :attribute/valueType)))
+                                          (mapcat (fn [field]
+                                                    [{:db/id "i dont think we need this"
+                                                      :anchor/prompt "edit"
+                                                      :anchor/link (system-edit-field-link find-element field parent-link)
+                                                      :anchor/repeating? true
+                                                      :anchor/find-element find-element
+                                                      :anchor/field field
+                                                      :anchor/formula (pr-str {:entity-dbid-s (pr-str '(fn [ctx] nil))})}])))))))
                        (into #{}))]
       (assoc parent-link :link/anchor anchors))
 
