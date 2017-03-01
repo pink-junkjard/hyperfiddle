1e70f684d4ee31bf9c9cfbe0cac75a3a03b61b97
Dustin Getz <dustin.getz@gmail.com>
2017-03-01 13:26:01 -0500

    repair option labels

--- src/hypercrud/form/option.cljs
+++ src/hypercrud/form/option.cljs
@@ -24,17 +24,17 @@
 (defn hydrate-options [field param-ctx]                     ; needs to return options as [[:db/id label]]
   (assert field)
   ; we are assuming we have a query link here
-  (let [request (-> field :field/options-anchor :anchor/link :link/request)]
+  (let [link (-> field :field/options-anchor :anchor/link)
+        request (-> link :link/request)]
     (mlet [q (exception/try-on (reader/read-string (:link-query/value request)))
-           resultset (let [params-map (merge (:query-params (links/build-url-params-map (:field/options-anchor field) param-ctx))
-                                             (q-util/build-dbhole-lookup request))
+           result (let [params-map (merge (:query-params (links/build-url-params-map (:field/options-anchor field) param-ctx))
+                                          (q-util/build-dbhole-lookup request))
                            query-value (q-util/query-value q request params-map param-ctx)]
                        (hc/hydrate (:peer param-ctx) query-value))]
-          (let [ordered-find-elements (find-elements-util/order-find-elements (:link-query/find-element request) q)
-                colspec (form-util/determine-colspec resultset ordered-find-elements param-ctx)]
+          (let [colspec (form-util/determine-colspec result link param-ctx)]
             (cats/return
-              (->> resultset
-                   (mapv (fn [result]
-                           (assert (= 1 (count ordered-find-elements)) "Cannot use multiple find-elements for an options-link; because you can't persist a ref to a tuple")
-                           (let [entity (get result (-> ordered-find-elements first :find-element/name))]
-                             [(:db/id entity) (build-label colspec result)])))))))))
+              (->> result
+                   (mapv (fn [relation]
+                           (let [[fe-name ident maybe-field] (first (partition 3 colspec))
+                                 entity (get relation fe-name)]
+                             [(:db/id entity) (build-label colspec relation)])))))))))
