591afd2f8a6a17a6f13c771dd102edca2fe4ceb0
Dustin Getz <dustin.getz@gmail.com>
2017-04-04 15:02:03 -0400

    tooltip scrap - noop

--- src/hypercrud/ui/tooltip.cljs
+++ src/hypercrud/ui/tooltip.cljs
@@ -3,7 +3,30 @@
             [re-com.core :as re-com]))
 
 
-(defn hover-tooltip [& props]
+;(defn tooltip [& {:as props}]                               ; re-com style props
+;  ; adapt standard props to re-com
+;  (let [props (-> props
+;                  (dissoc :body)                            ; leave :status and other props which don't need adaption
+;                  (merge {:label (:body props)}))]
+;    (apply re-com/popover-tooltip (apply concat props))))
+
+(defn popover-anchor-wrapper* [& args]
+  ; re-com has some weird "sugar" which inhibits dynamic props so turn it into a fn
+  (apply vector re-com/popover-anchor-wrapper args))
+
+;(defn popover [& {:as props}]
+;  (let [props (-> props
+;                  (dissoc :body)                            ; leave :status and other props which don't need adaption
+;                  (merge {:popover
+;                          [re-com/popover-content-wrapper
+;                           :on-cancel #(do (reset! state false) nil)
+;                           :no-clip? true
+;                           :body (:body props)]
+;
+;                          }))]
+;    (apply popover-anchor-wrapper* (apply concat props))))
+
+(defn hover-tooltip [& args]
   ; if there is no tooltip, the hovers are such that it will never be shown
   (let [state (r/atom false)]
     (fn [t-props anchor]
@@ -19,12 +42,7 @@
                              :on-mouse-leave #(do (if (:label t-props) (reset! state false)) nil)}
                       anchor]}))))))
 
-(defn popover-anchor-wrapper* [& args]
-  ; re-com has some weird "sugar" which inhibits dynamic props so turn it into a fn
-  (apply vector re-com/popover-anchor-wrapper args))
-
-
-(defn click-popover [& props]
+(defn click-popover [& args]
   (let [state (r/atom false)]
     (fn [t-props anchor]                                    ; magic :body key is documented at :popover :body
       (apply
@@ -42,8 +60,7 @@
                        :no-clip? true
                        :body (:body t-props)]}))))))
 
-
-(defn hover-popover [& props]
+(defn hover-popover* [& args]                               ; accept :label instead of :body to standardize
   (let [state (r/atom false)]
     (fn [t-props anchor]
       (apply
@@ -53,11 +70,13 @@
           (merge
             {:position :below-center
              :showing? state}
-            (dissoc t-props :body)
-            {:anchor [:div {:on-mouse-enter #(do (if (:body t-props) (reset! state true)) nil)
-                            :on-mouse-leave #(do (if (:body t-props) (reset! state false)) nil)}
+            (dissoc t-props :label :status)                 ; just ignore status, todo fix
+            {:anchor [:span {:on-mouse-enter #(do (if (:label t-props) (reset! state true)) nil)
+                             :on-mouse-leave #(do (if (:label t-props) (reset! state false)) nil)}
                       anchor]
              :popover [re-com/popover-content-wrapper
-                       :on-cancel #(do (reset! state false) nil)
                        :no-clip? true
-                       :body (:body t-props)]}))))))
+                       :body (:label t-props)]}))))))
+
+(defn hover-popover [& args]
+  (apply vector hover-popover* args))
