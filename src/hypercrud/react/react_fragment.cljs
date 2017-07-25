6685238187e8a096305cd63d5310676dc1e2deef
Dustin Getz <dustin.getz@gmail.com>
2017-07-25 16:31:41 -0400

    react fragment variable airity makes more sense

--- src/hypercrud/react/react_fragment.cljs
+++ src/hypercrud/react/react_fragment.cljs
@@ -1,5 +1,4 @@
 (ns hypercrud.react.react-fragment
   (:require [reagent.core :as r]))
 
-(defn react-fragment [xs & {k :react-key :or {k :_}}]
-  (js/reactCreateFragment (clj->js {k (map r/as-element xs)})))
+(defn react-fragment [react-key & xs] (js/reactCreateFragment (clj->js {react-key (map reagent.core/as-element xs)})))
