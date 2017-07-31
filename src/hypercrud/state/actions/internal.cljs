b0f66763b865c38a24124616a0b89932cb029555
Karl Hardenstine <khardenstine@gmail.com>
2017-07-31 13:51:20 -0400

    Allow request-fn to be parameterized

--- src/hypercrud/state/actions/internal.cljs
+++ src/hypercrud/state/actions/internal.cljs
@@ -9,19 +9,22 @@
 (defn batch [& action-list] (cons :batch action-list))
 
 ; force is a hack to be removed once this function runs in node
-(defn hydrate-until-queries-settle! [dispatch! get-state hydrate-id force]
-  (let [{:keys [entry-uri ptm stage] :as state} (get-state)
-        ; if the time to compute the requests is long
-        ; this peer could start returning inconsistent data compared to the state value,
-        ; however another hydrating action should have dispatched in that scenario,
-        ; so the resulting computation would be thrown away anyway
-        requests (into #{} (state/*request* state))
-        hydrated? (set/subset? (set requests) (set (keys ptm)))]
-    ; inspect dbvals used in requests see if stage has changed for them
-    (if (or force (not hydrated?))
-      (p/then (http/hydrate! entry-uri requests stage)
-              (fn [{:keys [t pulled-trees-map]}]
-                (when (= hydrate-id (:hydrate-id (get-state)))
-                  (dispatch! [:set-ptm pulled-trees-map])
-                  (hydrate-until-queries-settle! dispatch! get-state hydrate-id false))))
-      (p/resolved nil))))
+(defn hydrate-until-queries-settle!
+  ([dispatch! get-state hydrate-id force]
+    (hydrate-until-queries-settle! dispatch! get-state hydrate-id force state/*request*))
+  ([dispatch! get-state hydrate-id force request-fn]
+   (let [{:keys [entry-uri ptm stage] :as state} (get-state)
+         ; if the time to compute the requests is long
+         ; this peer could start returning inconsistent data compared to the state value,
+         ; however another hydrating action should have dispatched in that scenario,
+         ; so the resulting computation would be thrown away anyway
+         requests (into #{} (request-fn state))
+         hydrated? (set/subset? (set requests) (set (keys ptm)))]
+     ; inspect dbvals used in requests see if stage has changed for them
+     (if (or force (not hydrated?))
+       (p/then (http/hydrate! entry-uri requests stage)
+               (fn [{:keys [t pulled-trees-map]}]
+                 (when (= hydrate-id (:hydrate-id (get-state)))
+                   (dispatch! [:set-ptm pulled-trees-map])
+                   (hydrate-until-queries-settle! dispatch! get-state hydrate-id false request-fn))))
+       (p/resolved nil)))))
