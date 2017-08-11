b09ff2758ef1bfbd46345491395cacd336e83409
Karl Hardenstine <khardenstine@gmail.com>
2017-08-11 11:56:15 -0400

    Improve init-datomic bootstrapping

--- src/hypercrud/server/datomic/core.clj
+++ src/hypercrud/server/datomic/core.clj
@@ -7,9 +7,14 @@
 (defn init-datomic [transactor-uri]
   (let [root-uri (str transactor-uri "root")
         db-created? (d/create-database root-uri)]           ;idempotent
-    (assert (not db-created?) "Must seed with a real root-db, can't bootstrap from nothing anymore.")
+    (when db-created?
+      (d/delete-database root-uri)                          ; clean up
+      (throw (Error. "Must seed with a real root-db, can't bootstrap from nothing anymore.")))
 
     (alter-var-root #'db/transactor-uri (constantly transactor-uri))
-    (alter-var-root #'db/root-id (constantly
-                                   (d/q '[:find ?db . :where [?db :database/ident "root"]]
-                                        (d/db (database/get-root-conn)))))))
+    (if-let [root-id (d/q '[:find ?db . :where
+                            [?a :db/ident :database/ident]
+                            [?db ?a "root"]]
+                          (d/db (database/get-root-conn)))]
+      (alter-var-root #'db/root-id (constantly root-id))
+      (throw (Error. "Database registry not configured")))))
