ddd81d987db57d800cbfa1ea627d802b9cb0e63e
Dustin Getz <dustin.getz@gmail.com>
2017-08-25 14:50:15 -0400

    routing should not barf on query params

--- src/hypercrud/browser/routing.cljs
+++ src/hypercrud/browser/routing.cljs
@@ -15,9 +15,15 @@
 (defn decode [route-str]
   (assert (string/starts-with? route-str "/"))
 
-  (let [[_ project-name encoded-params] (string/split route-str #"/")]
+  ; Urls in the wild get query params added because tweetdeck tools think its safe e.g.:
+  ; http://localhost/hyperfiddle-blog/ezpkb21haW4gbmlsLCA6cHJvamVjdCAiaHlwZXJmaWRkbGUtYmxvZyIsIDpsaW5rLWRiaWQgI0RiSWRbMTc1OTIxODYwNDU4OTQgMTc1OTIxODYwNDU0MjJdLCA6cXVlcnktcGFyYW1zIHs6ZW50aXR5ICNEYklkWzE3NTkyMTg2MDQ2MTMyIDE3NTkyMTg2MDQ1ODgyXX19?utm_content=buffer9a24a&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer
+
+
+  (let [[_ project-name route-encoded-and-query-params] (string/split route-str #"/")]
     (cond
-      (not (nil? encoded-params)) (reader/read-string (base64/decode encoded-params))
+      (not (nil? route-encoded-and-query-params))
+      (let [[route-encoded url-param-garbage] (string/split route-encoded-and-query-params #"\?")]
+        (reader/read-string (base64/decode route-encoded)))
 
       ;deduce the project's index-link
       ;(not (nil? project-name)) {:project (unslugify project-name)}
