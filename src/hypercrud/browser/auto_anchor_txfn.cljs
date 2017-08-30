f06115524e35b203b992b42df98077e9196bcff5
Karl Hardenstine <khardenstine@gmail.com>
2017-08-30 19:25:12 -0400

    Split auto-tx-fns by dependent and create?; fix parent-child being used on dependent false

--- src/hypercrud/browser/auto_anchor_txfn.cljs
+++ src/hypercrud/browser/auto_anchor_txfn.cljs
@@ -9,23 +9,35 @@
   (letfn [(process-resource [code-str]
             (let [code-str (string/trim code-str)]
               (str-and-code' (eval/eval-str-and-throw code-str) code-str)))]
-    ; managing parent/child doesn't make sense without dependent true
-    {{:m true :fe true :a true} (-> (template/load-resource "auto-txfn/mt-fet-at.edn")
-                                    process-resource)
-     {:m true :fe true :a false} nil
+    (let [fe-dep-attr (process-resource (template/load-resource "auto-txfn/mt-fet-at.edn"))
 
-     {:m true :fe false :a true} nil
-     {:m true :fe false :a false} nil
+          fe {{:fe true :c? false :d? true :a false} nil
+              {:fe true :c? false :d? true :a true} fe-dep-attr
+              {:fe true :c? false :d? false :a false} nil
+              {:fe true :c? false :d? false :a true} nil
 
-     {:m false :fe true :a true} nil
-     {:m false :fe true :a false} nil
+              {:fe true :c? true :d? true :a false} nil
+              {:fe true :c? true :d? true :a true} fe-dep-attr
+              {:fe true :c? true :d? false :a false} nil
+              {:fe true :c? true :d? false :a true} nil}
 
-     {:m false :fe false :a true} nil
-     {:m false :fe false :a false} nil
-     }))
+          ; no fe = index or relation links
+          no-fe {{:fe false :c? false :d? true :a false} nil
+                 {:fe false :c? false :d? true :a true} nil
+                 {:fe false :c? false :d? false :a false} nil
+                 {:fe false :c? false :d? false :a true} nil
+
+                 {:fe false :c? true :d? true :a false} nil
+                 {:fe false :c? true :d? true :a true} nil
+                 {:fe false :c? true :d? false :a false} nil
+                 {:fe false :c? true :d? false :a true} nil}]
+      (merge fe no-fe))))
 
 (defn auto-txfn [anchor]
-  (get auto-tx-fn-lookup
-       {:m (or (:anchor/managed? anchor) false)
-        :fe (not (nil? (:anchor/find-element anchor)))
-        :a (not (nil? (:anchor/attribute anchor)))}))
+  (let [anchor-key {:fe (not (nil? (:anchor/find-element anchor)))
+                    :c? (or (:anchor/create? anchor) false)
+                    :d? (or (:anchor/repeating? anchor) false)
+                    :a (not (nil? (:anchor/attribute anchor)))}]
+    ; tx-fn is not applicable if the anchor is not managed
+    (if (or (:anchor/managed? anchor) false)
+      (get auto-tx-fn-lookup anchor-key))))
