69be3d198dc4731a259b8428c30d243afeab30e2
Karl Hardenstine <khardenstine@gmail.com>
2017-10-26 12:22:19 -0400

    Delete auto-control

--- src/hypercrud/ui/auto_control.cljs
+++
@@ -1,8 +0,0 @@
-(ns hypercrud.ui.auto-control)
-
-
-(defmulti auto-control (fn [] :default))
-(defmulti raw-control (fn [] :default))
-(defmulti auto-table-cell (fn [] :default))
-(defmulti raw-table-cell (fn [] :default))
-(defmulti result (fn [] :default))
