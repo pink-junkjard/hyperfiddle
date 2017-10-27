1ed399264195d633b139147de636abc1d84dfd5c
Dustin Getz <dustin.getz@gmail.com>
2017-10-27 18:48:16 -0400

    code-editor embedded in markdown

--- src/hypercrud/ui/markdown.cljs
+++ src/hypercrud/ui/markdown.cljs
@@ -1,10 +1,20 @@
 (ns hypercrud.ui.markdown
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [hypercrud.util.core :as util]
+            [hypercrud.ui.code-editor]))
 
-(defn MyComponent [] [:h3.bambi "bambi"])
 
-(def showdown (delay (js/ReactShowdown.Converter.
-                       (clj->js {"components" {"MyComponent" (r/reactify-component MyComponent)}}))))
+(defn code-editor-wrap-argv [{:keys [value change! props]}]
+  [hypercrud.ui.code-editor/code-editor* value change! props])
+
+(def whitelist
+  {"CodeEditor" code-editor-wrap-argv
+   "Bambi" (fn [] [:h3.bambi "bambi"])})
+
+(def showdown
+  (delay
+    (js/ReactShowdown.Converter.
+      (clj->js {"components" (util/map-values r/reactify-component whitelist)}))))
 
 (defn markdown [value change! & [props]]
   ; :dangerouslySetInnerHTML {:__html (.convert @showdown value)}
