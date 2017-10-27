9c43e4c3fe1c7a29d85d60cac7aba222f0f64029
Dustin Getz <dustin.getz@gmail.com>
2017-10-27 17:45:16 -0400

    React cmp in markdown working

--- src/hypercrud/ui/markdown.cljs
+++ src/hypercrud/ui/markdown.cljs
@@ -1,8 +1,10 @@
 (ns hypercrud.ui.markdown
   (:require [reagent.core :as r]))
 
-(def MyComponent (r/as-element [:pre.bambi "hello world"]))
-(def showdown (delay (js/ReactShowdown.Converter. #js {"MyComponent" MyComponent})))
+(defn MyComponent [] [:h3.bambi "bambi"])
+
+(def showdown (delay (js/ReactShowdown.Converter.
+                       (clj->js {"components" {"MyComponent" (r/reactify-component MyComponent)}}))))
 
 (defn markdown [value change! & [props]]
   ; :dangerouslySetInnerHTML {:__html (.convert @showdown value)}
