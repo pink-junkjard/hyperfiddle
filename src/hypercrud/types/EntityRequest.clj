ffcf4d90b4c3dd98602a2f0aaec6bd650a6c08fd
Karl Hardenstine <khardenstine@gmail.com>
2017-11-02 00:23:01 -0400

    Comments

--- src/hypercrud/types/EntityRequest.clj
+++ src/hypercrud/types/EntityRequest.clj
@@ -1,6 +1,7 @@
 (ns hypercrud.types.EntityRequest)
 
 
+; todo fix db <-> dbval naming between server/client
 (defrecord EntityRequest [e a dbval pull-exp])
 
 (def read-EntityRequest #(apply ->EntityRequest %))
