54c4077e22fdd918764314810651af25fd0093b8
Karl Hardenstine <khardenstine@gmail.com>
2017-11-20 17:21:17 -0500

    Hack back support for field docstrings
    hyperfiddle/hyperfiddle#41

--- src/hypercrud/browser/find_element.cljs
+++ src/hypercrud/browser/find_element.cljs
@@ -21,7 +21,7 @@
        :splat? false
        :type :variable})))
 
-(defn pull-cell->fe [cell source-symbol fe-name pull-pattern]
+(defn pull-cell->fe [docs-lookup cell source-symbol fe-name pull-pattern]
   (let [splat? (not (empty? (filter #(= '* %) pull-pattern)))]
     (map->FindElement
       {:name fe-name
@@ -40,13 +40,13 @@
                       (mapv (fn [attr]
                               (map->Field {:id (hash [fe-name attr])
                                            :attribute attr
-                                           :doc nil
+                                           :doc (get docs-lookup attr)
                                            :cell-data->value attr})))))
        :source-symbol source-symbol
        :splat? splat?
        :type :pull})))
 
-(defn pull-many-cells->fe [column-cells source-symbol fe-name pull-pattern]
+(defn pull-many-cells->fe [docs-lookup column-cells source-symbol fe-name pull-pattern]
   (let [splat? (not (empty? (filter #(= '* %) pull-pattern)))]
     (map->FindElement
       {:name fe-name
@@ -71,7 +71,7 @@
                       (mapv (fn [attr]
                               (map->Field {:id (hash [fe-name attr])
                                            :attribute attr
-                                           :doc nil
+                                           :doc (get docs-lookup attr)
                                            :cell-data->value attr})))))
        :source-symbol source-symbol
        :splat? splat?
@@ -91,13 +91,13 @@
        :splat? false
        :type :aggregate})))
 
-(defn auto-fe-one-cell [element cell]
+(defn auto-fe-one-cell [docs-lookup element cell]
   (condp = (type element)
     datascript.parser.Variable
     (variable->fe element)
 
     datascript.parser.Pull
-    (pull-cell->fe cell
+    (pull-cell->fe docs-lookup cell
                    (get-in element [:source :symbol])
                    (get-in element [:variable :symbol])
                    (get-in element [:pattern :value]))
@@ -105,13 +105,13 @@
     datascript.parser.Aggregate
     (aggregate->fe element)))
 
-(defn auto-fe-many-cells [element column-cells]
+(defn auto-fe-many-cells [docs-lookup element column-cells]
   (condp = (type element)
     datascript.parser.Variable
     (variable->fe element)
 
     datascript.parser.Pull
-    (pull-many-cells->fe column-cells
+    (pull-many-cells->fe docs-lookup column-cells
                          (get-in element [:source :symbol])
                          (get-in element [:variable :symbol])
                          (get-in element [:pattern :value]))
@@ -119,28 +119,39 @@
     datascript.parser.Aggregate
     (aggregate->fe element)))
 
+(defn build-docs-lookup [ctx]
+  (reduce (fn [acc fe]
+            (reduce (fn [acc field]
+                      (assoc acc (:field/attribute field) (:db/doc field)))
+                    acc
+                    (get-in fe [:find-element/form :form/field])))
+          {}
+          (get-in ctx [:fiddle :link-query/find-element])))
+
 (defn auto-find-elements [result ctx]
   (case (get-in ctx [:fiddle :request/type])
     :entity (mlet [source-symbol (try-either (.-dbname (get-in ctx [:route :request-params :entity])))
                    :let [fe-name "entity"
-                         pull-pattern (get-in ctx [:request :pull-exp])]]
+                         pull-pattern (get-in ctx [:request :pull-exp])
+                         docs-lookup (build-docs-lookup ctx)]]
               (cats/return
                 (if-let [a (get-in ctx [:request :a])]
                   (case (get-in ctx [:schemas (str source-symbol) a :db/cardinality :db/ident])
                     :db.cardinality/one
-                    [(pull-cell->fe result source-symbol fe-name pull-pattern)]
+                    [(pull-cell->fe docs-lookup result source-symbol fe-name pull-pattern)]
 
                     :db.cardinality/many
-                    [(pull-many-cells->fe result source-symbol fe-name pull-pattern)])
-                  [(pull-cell->fe result source-symbol fe-name pull-pattern)])))
+                    [(pull-many-cells->fe docs-lookup result source-symbol fe-name pull-pattern)])
+                  [(pull-cell->fe docs-lookup result source-symbol fe-name pull-pattern)])))
 
-    :query (mlet [{:keys [qfind]} (try-either (parser/parse-query (get-in ctx [:request :query])))]
+    :query (mlet [{:keys [qfind]} (try-either (parser/parse-query (get-in ctx [:request :query])))
+                  :let [docs-lookup (build-docs-lookup ctx)]]
              (cats/return
                (condp = (type qfind)
-                 datascript.parser.FindRel (mapv auto-fe-many-cells (:elements qfind) (apply map vector result))
-                 datascript.parser.FindColl [(auto-fe-many-cells (:element qfind) result)]
-                 datascript.parser.FindTuple (mapv auto-fe-one-cell (:elements qfind) result)
-                 datascript.parser.FindScalar [(auto-fe-one-cell (:element qfind) result)])))
+                 datascript.parser.FindRel (mapv (partial auto-fe-many-cells docs-lookup) (:elements qfind) (apply map vector result))
+                 datascript.parser.FindColl [(auto-fe-many-cells docs-lookup (:element qfind) result)]
+                 datascript.parser.FindTuple (mapv (partial auto-fe-one-cell docs-lookup) (:elements qfind) result)
+                 datascript.parser.FindScalar [(auto-fe-one-cell docs-lookup (:element qfind) result)])))
 
     :blank (either/right [])
 
