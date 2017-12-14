71e000ca232427267664143ebf8a4194f7dc6ad9
Karl Hardenstine <khardenstine@gmail.com>
2017-12-14 11:57:09 -0500

    Platform agnostic base-64
    hyperfiddle/hyperfiddle#100

--- src-browser/hypercrud/platform/base_64.cljs
+++
@@ -1,9 +0,0 @@
-(ns hypercrud.platform.base-64
-  (:require [goog.crypt.base64 :as base64]))
-
-
-(defn encode-string [s]
-  (base64/encodeString s))
-
-(defn decode-string [s]
-  (base64/decodeString s))
