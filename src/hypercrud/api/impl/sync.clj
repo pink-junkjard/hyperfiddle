3cd86f5877d2cbd47ec0e0dd7460ae342e357235
Karl Hardenstine <khardenstine@gmail.com>
2017-12-22 16:34:50 -0500

    Sync returns sorted-map
    hyperfiddle/hyperfiddle.net#122

--- src/hypercrud/api/impl/sync.clj
+++ src/hypercrud/api/impl/sync.clj
@@ -1,8 +1,12 @@
 (ns hypercrud.api.impl.sync
   (:refer-clojure :exclude [sync])
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [taoensso.timbre :as timbre]))
 
 
 (defn sync [dbs]                                            ; sync is the low level datomic call
+  (timbre/debug "syncing" (pr-str dbs))
   ; ordered kv seq
-  (->> dbs (map (juxt identity #(-> % str d/connect d/sync deref d/basis-t)))))
+  (->> dbs
+       (mapcat (juxt identity #(-> % str d/connect d/sync deref d/basis-t)))
+       (apply sorted-map)))
