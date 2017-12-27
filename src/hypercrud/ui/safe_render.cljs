4ea4433c06ed4680e600b8971ac343d395382bd3
Dustin Getz <dustin.getz@gmail.com>
2017-12-27 14:56:39 -0500

    userroot classname

--- src/hypercrud/ui/safe_render.cljs
+++ src/hypercrud/ui/safe_render.cljs
@@ -15,9 +15,9 @@
 (code-for-nodejs
   (defn safe-user-renderer [user-fn & props]
     (let [user-result (apply vector user-fn props)
-          explicit-container [:div.hyperfiddle-internal-react-root {:key (hash user-fn)}
+          explicit-container [:div.hyperfiddle-userroot {:key (hash user-fn)}
                               user-result]]
-      [:div.hyperfiddle-internal-react-portal
+      [:div.hyperfiddle-userportal
        {:dangerouslySetInnerHTML
         {:__html
          (try
@@ -34,7 +34,7 @@
           ; but not hiccup syntax, which is necessary to work with reagent-style
           ; components with closure-constructor pattern.
           user-result (apply vector user-fn props)
-          explicit-container [:div.hyperfiddle-internal-react-root {:key (hash user-fn)}
+          explicit-container [:div.hyperfiddle-userroot {:key (hash user-fn)}
                               user-result]]
       (try
         (reagent/render explicit-container dom-el)
@@ -43,7 +43,7 @@
 
   (def safe-user-renderer
     (reagent/create-class
-      {:reagent-render (fn [user-fn & props] [:div.hyperfiddle-internal-react-portal])
+      {:reagent-render (fn [user-fn & props] [:div.hyperfiddle-userportal])
        :component-did-mount safe-render!
        :component-will-unmount
        (fn [this]
@@ -51,8 +51,8 @@
        :component-did-update safe-render!})))
 
 (defn foo [control & args]
-  [:div.hyperfiddle-internal-react-portal
-   [:div.hyperfiddle-internal-react-root
+  [:div.hyperfiddle-userportal
+   [:div.hyperfiddle-userroot
     (apply vector control args)]])
 
 (defn unify-portal-markup [control]
