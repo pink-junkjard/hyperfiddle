67bea1c452cf0061b2263953ca7f0604c2e3c458
Dustin Getz <dustin.getz@gmail.com>
2018-01-01 12:22:19 -0500

    add markdown block plugin

--- src/hypercrud/ui/control/markdown_rendered.cljs
+++ src/hypercrud/ui/control/markdown_rendered.cljs
@@ -5,6 +5,8 @@
             [hypercrud.compile.eval :as eval]))
 
 
+(declare markdown)
+
 (defn code-editor-wrap-argv [{:keys [value change!] :as props}]
   [hypercrud.ui.control.code/code* value change! props])
 
@@ -12,16 +14,21 @@
   {"span" (fn [props] [:span (dissoc props :children :value) (:value props)])
    ; Div is not needed, use it with block syntax and it hits React.createElement and works
    ; see https://github.com/medfreeman/remark-generic-extensions/issues/30
-   "CodeEditor" code-editor-wrap-argv})
+   "CodeEditor" code-editor-wrap-argv
+   "block" (fn [props] [:div (dissoc props :children :value) (markdown (:value props))])})
 
 ; https://github.com/medfreeman/remark-generic-extensions
+; https://github.com/zestedesavoir/zmarkdown/tree/master/packages/remark-grid-tables
+; https://github.com/zestedesavoir/zmarkdown/tree/master/packages/remark-custom-blocks
 
 (def remarkInstance (-> (js/remark)
+                        ;(.use js/remarkCustomBlocks (clj->js {"some" "a"}))
                         (.use js/remarkGenericExtensions
                               (clj->js
                                 {"elements"
                                  {"span" {"html" {"properties" {"value" "::content::"}}}
-                                  "CodeEditor" {"html" {"properties" {"value" "::content::"}}}}}))
+                                  "CodeEditor" {"html" {"properties" {"value" "::content::"}}}
+                                  "block" {"html" {"properties" {"value" "::content::"}}}}}))
                         (.use js/remarkReact
                               (clj->js
                                 {"sanitize" false
