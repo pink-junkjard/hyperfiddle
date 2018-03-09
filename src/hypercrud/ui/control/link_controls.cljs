fa5c9987899ef6c5dd6d77b015c63760a59733d4
Karl Hardenstine <khardenstine@gmail.com>
2018-03-09 15:00:22 -0500

    Capture reactions tighter on links #7

--- src/hypercrud/ui/control/link_controls.cljs
+++ src/hypercrud/ui/control/link_controls.cljs
@@ -11,15 +11,22 @@
           "_")
       str))
 
+; garbage wrapper for reactivity capturing
+(defn- reactive-nav-cmp [link-ref ctx]
+  [(:navigate-cmp ctx) (link/build-link-props @link-ref ctx) @(reactive/track prompt link-ref)])
+
 ; ideally in the future we can infer path and dependent? from the ctx
 (defn render-nav-cmps [path dependent? ctx & processors]
   (let [links (reactive/track link-controls-util/ui-contextual-links path dependent? false (:hypercrud.browser/links ctx) processors)]
     (->> (reactive/unsequence :db/id links)
          (map (fn [[link-ref link-id]]
-                (let [prompt @(reactive/track prompt link-ref)]
-                  ^{:key (hash link-id)}
-                  [(:navigate-cmp ctx) (link/build-link-props @link-ref ctx) prompt])))
+                ^{:key (hash link-id)}
+                [reactive-nav-cmp link-ref ctx]))
          (doall))))
+
+; garbage wrapper for reactivity capturing
+(defn- reactive-ui [link-ref ctx]
+  [browser/ui @link-ref ctx])
 
 ; ideally in the future we can infer path and dependent? from the ctx
 (defn render-inline-links [path dependent? ctx & processors]
@@ -29,5 +36,5 @@
               ; don't test link validity, we need to render the failure. If this is a dependent link, use visibility predicate to hide the error.
               [:div {:key (hash link-id)}                   ; extra div bc had trouble getting keys to work
                ; NOTE: this ctx logic and structure is the same as the inline branch of browser-request/recurse-request
-               [browser/ui @link-ref ctx]]))
+               [reactive-ui link-ref ctx]]))
        (doall)))
