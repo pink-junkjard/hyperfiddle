63ed0dc83830ded5c25facf27156eeb79f55812e
Karl Hardenstine <khardenstine@gmail.com>
2018-03-09 12:41:04 -0500

    Remove attribute renderer’s dead :link-fn #46

--- src/hypercrud/ui/user_attribute_renderer.cljs
+++ src/hypercrud/ui/user_attribute_renderer.cljs
@@ -1,22 +1,9 @@
 (ns hypercrud.ui.user-attribute-renderer
   (:require [cats.monad.either :as either]
-            [hypercrud.browser.link :as link]
             [hypercrud.compile.eval :as eval]
             [hypercrud.ui.safe-render :refer [safe-reagent-call]]
-            [hypercrud.util.core :refer [pprint-str]]
-            [hypercrud.util.reactive :as reactive]))
+            [hypercrud.util.core :refer [pprint-str]]))
 
-
-(defn- lookup-link [ctx rel]
-  (->> @(:hypercrud.brower/links ctx)
-       (filter #(= (:link/rel %) rel))
-       (map (fn [link]
-              ; backwards compat, why is this a thing for custom user controls?
-              (if (link/popover-link? link)
-                (assoc link :link/render-inline? false)
-                link)))
-       (filter (link/same-path-as? [(:fe-pos ctx) (:hypercrud.browser/attribute ctx)]))
-       first))
 
 (defn safe-eval-user-control-fn [s]
   (-> (eval/eval-str s)
@@ -25,16 +12,7 @@
         (fn r [f]
           (when f
             (fn user-control [field props ctx]
-              (let [lookup-link (reactive/partial lookup-link ctx)
-                    ctx (assoc ctx                          ; Todo unify link-fn with widget interface or something
-                          :link-fn
-                          (fn [ident label ctx]
-                            (let [link @(reactive/track lookup-link ident)
-                                  props (link/build-link-props link ctx)] ; needs ctx to run formulas
-                              [(:navigate-cmp ctx) props label])))]
-                ; Same interface as auto-control widgets.
-                ; pass value only as scope todo
-                [safe-reagent-call f field props ctx])))))))
+              [safe-reagent-call f field props ctx]))))))
 
 (defn safe-eval-user-expr [s]
   (-> (eval/eval-str s)
