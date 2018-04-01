e733bde61b1c2c26dcf9cb9d3344e64770cd9def
Dustin Getz <dustin.getz@gmail.com>
2018-04-01 16:44:44 -0400

    regression: markdown codemirror elements are busted #151

--- src/hypercrud/ui/control/markdown_rendered.cljs
+++ src/hypercrud/ui/control/markdown_rendered.cljs
@@ -13,80 +13,10 @@
             [reagent.core :as reagent]))
 
 
-(declare markdown)
-(declare markdown-relation)
 (declare remarkInstance)
-
-(defn code-editor-wrap-argv [{:keys [value change!] :as props}]
-  [hypercrud.ui.control.code/code* value change! props])
 
 ; Reagent + react-context:
 ; https://github.com/reagent-project/reagent/commit/a8ec0d219bbd507f51a4d9276c4a1dcc020245af
-
-(defn md-extension [f]
-  (reagent/create-class
-    {:context-types #js {:ctx js/propTypes.object}
-     :reagent-render (fn [{:keys [content argument] :as props}]
-                       (let [ctx (goog.object/get (.-context (reagent/current-component)) "ctx")
-                             content (if-not (= content "undefined") content)
-                             argument (if-not (= argument "undefined") argument)]
-                         (f content argument (dissoc props :content :argument) ctx)))}))
-
-(def eval
-  (md-extension
-    (fn [content argument props ctx]
-      (binding [hyperfiddle.core/*ctx* ctx]
-        (safe-eval-user-expr content)))))
-
-(def browse
-  (md-extension
-    (fn [content argument props ctx]
-      (let [kwargs (flatten (seq props))]
-        (apply (:browse ctx) (keyword argument) ctx kwargs)))))
-
-(def anchor
-  (md-extension
-    (fn [content argument props ctx]
-      (let [kwargs (flatten (seq props))
-            ; https://github.com/medfreeman/remark-generic-extensions/issues/45
-            label (or-str content argument)]
-        (apply (:anchor ctx) (keyword argument) ctx label kwargs)))))
-
-(def cell
-  (md-extension
-    (fn [content argument props ctx]
-      (let [kwargs (flatten (seq props))
-            path (into [true] (unwrap (memoized-safe-read-edn-string (str "[" argument "]"))))]
-        (apply (:cell ctx) path ctx kwargs)))))
-
-(def table
-  (md-extension
-    (fn [content argument {:keys [class] :as props} ctx]
-      (let [kwargs (flatten (seq (dissoc props :class)))]
-        (hypercrud.ui.table/Table ctx)))))
-
-(def list-
-  (md-extension
-    (letfn [(keyfn [relation] (hash (map #(or (:db/id %) %) relation)))]
-      (fn [content argument {:keys [class] :as props} ctx]
-        [:div {:class (classes class)}
-         (->> (:relations ctx)
-              (r/unsequence keyfn)
-              (map (fn [[relation k]]
-                     ^{:key k} [markdown-relation k content (context/relation ctx relation)]))
-              (doall))]))))
-
-(def value
-  (md-extension
-    (fn [content argument props ctx]
-      (let [content (if content (safe-eval-user-expr content))
-            path (into [true] (unwrap (memoized-safe-read-edn-string (str "[" argument "]"))))]
-        (fragment path ((:value ctx) path ctx content))))))
-
-#_(fragment
-    k
-    (binding [hyperfiddle.core/*ctx* (context/relation ctx relation)]
-      (safe-eval-user-expr content)))                       ; (fn f [content ctx & [?class]])
 
 (def markdown
   ; remark creates react components which don't evaluate in this stack frame
@@ -105,19 +35,76 @@
 
      :child-context-types #js {:ctx js/propTypes.object}}))
 
+(defn md-extension [f]
+  (reagent/create-class
+    {:context-types #js {:ctx js/propTypes.object}
+     :reagent-render (fn [{:keys [content argument] :as props}]
+                       (let [ctx (goog.object/get (.-context (reagent/current-component)) "ctx")
+                             content (if-not (= content "undefined") content)
+                             argument (if-not (= argument "undefined") argument)]
+                         (f content argument (dissoc props :content :argument) ctx)))}))
+
+
+(declare markdown-relation)
+
+(defn code-editor-wrap-argv [content argument props ctx]
+  [hypercrud.ui.control.code/code* content #() props])
+
+#_(fragment
+    k
+    (binding [hyperfiddle.core/*ctx* (context/relation ctx relation)]
+      (safe-eval-user-expr content)))                       ; (fn f [content ctx & [?class]])
+
+(defn eval [content argument props ctx]
+  (binding [hyperfiddle.core/*ctx* ctx]
+    (safe-eval-user-expr content)))
+
+(defn browse [content argument props ctx]
+  (let [kwargs (flatten (seq props))]
+    (apply (:browse ctx) (keyword argument) ctx kwargs)))
+
+(defn anchor [content argument props ctx]
+  (let [kwargs (flatten (seq props))
+        ; https://github.com/medfreeman/remark-generic-extensions/issues/45
+        label (or-str content argument)]
+    (apply (:anchor ctx) (keyword argument) ctx label kwargs)))
+
+(defn cell [content argument props ctx]
+  (let [kwargs (flatten (seq props))
+        path (into [true] (unwrap (memoized-safe-read-edn-string (str "[" argument "]"))))]
+    (apply (:cell ctx) path ctx kwargs)))
+
+(defn table [content argument {:keys [class] :as props} ctx]
+  (let [kwargs (flatten (seq (dissoc props :class)))]
+    (hypercrud.ui.table/Table ctx)))
+
+(letfn [(keyfn [relation] (hash (map #(or (:db/id %) %) relation)))]
+  (defn list- [content argument {:keys [class] :as props} ctx]
+    [:div {:class (classes class)}
+     (->> (:relations ctx)
+          (r/unsequence keyfn)
+          (map (fn [[relation k]]
+                 ^{:key k} [markdown-relation k content (context/relation ctx relation)]))
+          (doall))]))
+
+(defn value [content argument props ctx]
+  (let [content (if content (safe-eval-user-expr content))
+        path (into [true] (unwrap (memoized-safe-read-edn-string (str "[" argument "]"))))]
+    (fragment path ((:value ctx) path ctx content))))
+
 (def whitelist-reagent
   ; Div is not needed, use it with block syntax and it hits React.createElement and works
   ; see https://github.com/medfreeman/remark-generic-extensions/issues/30
-  {"span" (md-extension (fn [content argument props ctx]
-                          [:span (dissoc props :children) content]))
-   "p" (md-extension (fn [content argument props ctx]
-                       [:div (-> props
-                                 (dissoc :children)
-                                 (update :class #(classes "p" %)))
-                        (:children props)]))
+  {"span" (fn [content argument props ctx]
+            [:span (dissoc props :children) content])
+   "p" (fn [content argument props ctx]
+         [:div (-> props
+                   (dissoc :children)
+                   (update :class #(classes "p" %)))
+          (:children props)])
    "CodeEditor" code-editor-wrap-argv
-   "block" (md-extension (fn [content argument props ctx]
-                           [:div props [markdown content]]))
+   "block" (fn [content argument props ctx]
+             [:div props [markdown content]])
    "cljs" eval
    "browse" browse
    "anchor" anchor
@@ -155,7 +142,10 @@
                         (.use js/remarkReact
                               (clj->js
                                 {"sanitize" false
-                                 "remarkReactComponents" (->> whitelist-reagent (map-values reagent/reactify-component))}))))
+                                 "remarkReactComponents" (->> whitelist-reagent
+                                                              (map-values
+                                                                (comp reagent/reactify-component
+                                                                      md-extension)))}))))
 
 
 ; Todo; remove div.markdown; that should be default and style the inverse.
