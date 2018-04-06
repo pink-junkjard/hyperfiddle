caa1c08c1f012a1c1586639f55d69bb3eee0e6b7
Karl Hardenstine <khardenstine@gmail.com>
2018-04-06 19:33:03 -0400

    Drop user-root #165

--- src/hypercrud/ui/safe_render.cljs
+++ src/hypercrud/ui/safe_render.cljs
@@ -14,16 +14,13 @@
 
 (code-for-nodejs
   (defn safe-reagent-call [user-fn & props]
-    (let [user-result (apply vector user-fn props)
-          explicit-container [:div.hyperfiddle-userroot {:key (hash user-fn)}
-                              user-result]]
-      [:div.hyperfiddle-userportal
-       {:dangerouslySetInnerHTML
-        {:__html
-         (try
-           (reagent-server/render-to-string explicit-container)
-           (catch js/Error e
-             (reagent-server/render-to-string [:pre (pr-str e)])))}}])))
+    [:div.hyperfiddle-userportal
+     {:dangerouslySetInnerHTML
+      {:__html
+       (try
+         (reagent-server/render-to-string (apply vector user-fn props))
+         (catch js/Error e
+           (reagent-server/render-to-string [:pre (pr-str e)])))}}]))
 
 (code-for-browser
   (defn safe-reagent-call [user-fn & props]
@@ -33,19 +30,17 @@
                            (if-let [e @e-state]
                              [:pre (pr-str e)]              ; todo inject error cmp
                              [:div.hyperfiddle-userportal
-                              [:div.hyperfiddle-userroot {:key (hash user-fn)}
-                               ; if we comment out "vector" it works with react-fragments
-                               ; but not hiccup syntax, which is necessary to work with reagent-style
-                               ; components with closure-constructor pattern.
-                               (apply vector user-fn props)]]))
+                              ; if we comment out "vector" it works with react-fragments
+                              ; but not hiccup syntax, which is necessary to work with reagent-style
+                              ; components with closure-constructor pattern.
+                              (apply vector user-fn props)]))
 
          :component-did-catch (fn [#_this e info]           ; args will need updating in reagent0.8.x
                                 (reset! e-state e))}))))
 
 (defn foo [control & args]
   [:div.hyperfiddle-userportal
-   [:div.hyperfiddle-userroot
-    (apply vector control args)]])
+   (apply vector control args)])
 
 (defn unify-portal-markup [control]
   (assert control)
