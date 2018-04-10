0fdec71f5906e73def4cdcbe102c5e12698ea88d
Karl Hardenstine <khardenstine@gmail.com>
2018-04-10 14:49:42 -0400

    browser_ui is cljs

--- src/hypercrud/browser/browser_ui.cljs
+++ src/hypercrud/browser/browser_ui.cljs
@@ -4,17 +4,16 @@
             [contrib.css :refer [css-slugify classes]]
             [contrib.data :as util :refer [unwrap]]
             [contrib.reactive :as r]
-    #?(:cljs [contrib.reagent :refer [fragment]])
             [contrib.string :refer [memoized-safe-read-edn-string]]
             [contrib.try :refer [try-either]]
             [hypercrud.browser.base :as base]
             [hypercrud.browser.context :as context]
             [hypercrud.browser.link :as link]
             [hypercrud.browser.routing :as routing]
-    #?(:cljs [hypercrud.ui.error :as ui-error])
-    ;#?(:cljs [hypercrud.ui.form :as form])
+            [hypercrud.ui.error :as ui-error]
+    ; [hypercrud.ui.form :as form]
             [hypercrud.ui.native-event-listener :refer [native-on-click-listener]]
-    #?(:cljs [hypercrud.ui.safe-render :refer [safe-reagent-call]])
+            [hypercrud.ui.safe-render :refer [safe-reagent-call]]
             [hypercrud.ui.stale :as stale]
             [hyperfiddle.foundation :as foundation]
             [hyperfiddle.foundation.actions :as foundation-actions]
@@ -35,20 +34,18 @@
 (defn f-mode-config []
   {:from-ctx :user-renderer
    :from-fiddle (fn [fiddle] @(r/cursor fiddle [:fiddle/renderer]))
-   :with-user-fn #?(:clj  (assert false "todo")
-                    :cljs (fn [user-fn]
-                            (fn [ctx]
-                              #_(fragment :_) #_(list)
-                              [:div
-                               [safe-reagent-call (ui-error/error-comp ctx) user-fn ctx (auto-ui-css-class ctx)]
-                               [fiddle-css-renderer @(r/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/css])]])))
+   :with-user-fn (fn [user-fn]
+                   (fn [ctx]
+                     #_(fragment :_) #_(list)
+                     [:div
+                      [safe-reagent-call (ui-error/error-comp ctx) user-fn ctx (auto-ui-css-class ctx)]
+                      [fiddle-css-renderer @(r/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/css])]]))
    ; todo ui binding should be provided by a RT
-   :default #?(:clj  (assert false "todo")
-               :cljs (fn [ctx]
-                       #_(fragment :_) #_(list)
-                       [:div
-                        [hypercrud.ui.result/fiddle-xray ctx (auto-ui-css-class ctx)]
-                        [fiddle-css-renderer @(r/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/css])]]))})
+   :default (fn [ctx]
+              #_(fragment :_) #_(list)
+              [:div
+               [hypercrud.ui.result/fiddle-xray ctx (auto-ui-css-class ctx)]
+               [fiddle-css-renderer @(r/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/css])]])})
 
 (letfn [(browse [rel #_dependent? path ctx & args]
           (let [{[user-renderer & args] nil :as kwargs} (util/kwargs args)
@@ -64,14 +61,13 @@
                           #_(dissoc :style) #_"custom renderers don't want colored links")]
             [(:navigate-cmp ctx) props label (:class kwargs)]))
         (cell [[d i a] ctx & args]                          ; form only
-          #?(:clj nil
-             :cljs [hypercrud.ui.form/Cell (context/relation-path ctx [d i a])]))
+          [hypercrud.ui.form/Cell (context/relation-path ctx [d i a])])
         (value [path ctx & args]
           (let [{[f & args] nil :as kwargs} (util/kwargs args)
                 ctx (context/relation-path ctx path)
                 field (:hypercrud.browser/field ctx)
                 #_#_control-props (merge (hypercrud.ui.auto-control/control-props ctx) kwargs)]
-            #?(:cljs [(or f (partial hypercrud.ui.auto-control/auto-control field {} nil)) ctx])))
+            [(or f (partial hypercrud.ui.auto-control/auto-control field {} nil)) ctx]))
         (browse' [rel #_dependent? path ctx]
           (->> (base/data-from-link @(r/track link/rel->link rel path ctx) ctx)
                (cats/fmap :hypercrud.browser/result)
@@ -100,8 +96,7 @@
                             route)
         either-v (or (some-> @(runtime/state (:peer ctx) [::runtime/partitions (:branch ctx) :error]) either/left)
                      either-v)
-        error-comp #?(:cljs (ui-error/error-comp ctx)
-                      :clj  (assert false "todo"))]
+        error-comp (ui-error/error-comp ctx)]
     [native-on-click-listener {:on-click on-click}
      [stale/loading (stale/can-be-loading? ctx) either-v
       (fn [e] [:div {:class (classes "ui" class "hyperfiddle-error")} [error-comp e]])
--- src/hypercrud/browser/core.cljc
+++ src/hypercrud/browser/core.cljc
@@ -1,17 +1,17 @@
 (ns hypercrud.browser.core
   (:require [hypercrud.browser.base :as base]
             [hypercrud.browser.browser-request :as browser-request]
-            [hypercrud.browser.browser-ui :as browser-ui]))
+    #?(:cljs [hypercrud.browser.browser-ui :as browser-ui])))
 
 
 (def data base/data-from-link)
-(def ui browser-ui/ui-from-link)
+#?(:cljs (def ui browser-ui/ui-from-link))
 (def request browser-request/request-from-link)
 
 ; this interface is to reuse an evaluated route (performance of eval and effects)
 ; and also the entrypoint (route is document.location, original anchor is unknowable)
 (def data-from-route base/data-from-route)
-(def ui-from-route browser-ui/ui-from-route)
+#?(:cljs (def ui-from-route browser-ui/ui-from-route))
 (def request-from-route browser-request/request-from-route)
 
 
