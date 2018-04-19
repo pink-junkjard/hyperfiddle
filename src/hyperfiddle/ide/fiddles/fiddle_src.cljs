898d7e442ceb20e3783cb66ae734d3afc9fe12ea
Dustin Getz <dustin.getz@gmail.com>
2018-04-19 16:00:31 -0400

    Improve user experience of srcmode #207

--- src/hyperfiddle/ide/fiddles/fiddle_src.cljs
+++ src/hyperfiddle/ide/fiddles/fiddle_src.cljs
@@ -6,14 +6,27 @@
             [hyperfiddle.ide.fiddles.topnav :refer [shadow-fiddle hijack-renderer loading-spinner]]))
 
 
-(defn fiddle-src-renderer [ctx]
+(defn fiddle-src-renderer [ctx class]
+  #_(hypercrud.ui.result/fiddle ctx)
+  #_[:div {:class class}
+     [:h3 "fiddle src"]
+     [hypercrud.ui.control.markdown-rendered/markdown (-> ctx :hypercrud.browser/fiddle deref :db/doc)]
+     [hypercrud.ui.control.markdown-rendered/markdown "!result[]" ctx]
+     ((:browse ctx) :attribute-renderers [] ctx (partial hijack-renderer true))
+     ((:browse ctx) :links [] ctx (partial hijack-renderer true))]
+
   (let [#_#_ctx (shadow-fiddle ctx)
         rtype (:fiddle/type @(:hypercrud.browser/result ctx))]
-    [:div
+    [:div {:class class}
+     [:h3 "fiddle src"]
      ((:cell ctx) [true 0 :fiddle/type] ctx)
      (case rtype
        :entity ((:cell ctx) [true 0 :fiddle/pull] ctx)
        :query ((:cell ctx) [true 0 :fiddle/query] ctx)
-       nil)
-     ((:browse ctx) :ui [] ctx (partial hijack-renderer false))
+       :blank nil
+       nil nil)
+     ((:cell ctx) [true 0 :fiddle/markdown] ctx)
+     ((:cell ctx) [true 0 :fiddle/renderer] ctx)
+     ((:cell ctx) [true 0 :fiddle/css] ctx)
+     ((:browse ctx) :attribute-renderers [] ctx (partial hijack-renderer true))
      ((:browse ctx) :links [] ctx (partial hijack-renderer true))]))
