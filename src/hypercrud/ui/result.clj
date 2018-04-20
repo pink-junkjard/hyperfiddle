e733ac850f3d70c2b48905a53a8a70e4836453e6
Dustin Getz <dustin.getz@gmail.com>
2018-04-20 18:20:13 -0400

    fix result template for link-controls rename

--- src/hypercrud/ui/result.clj
+++ src/hypercrud/ui/result.clj
@@ -7,9 +7,9 @@
   (let [expr '[:div {:class (contrib.css/classes "auto-result" class)} ; auto-result ?
                [:h3 (some-> ctx :hypercrud.browser/fiddle deref :fiddle/ident name)]
                [hypercrud.ui.control.markdown-rendered/markdown (-> ctx :hypercrud.browser/fiddle deref :db/doc)]
-               (hypercrud.ui.control.link-controls/render-nav-cmps [] false ctx :class "hyperfiddle-link-index")
+               (hypercrud.ui.control.link-controls/anchors [] false ctx :class "hyperfiddle-link-index")
                (let [content (contrib.string/or-str @(contrib.reactive/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/markdown]) "!result[]")]
                  [hypercrud.ui.control.markdown-rendered/markdown content ctx])
-               (hypercrud.ui.control.link-controls/render-inline-links [] false ctx)]]
+               (hypercrud.ui.control.link-controls/iframes [] false ctx)]]
     `(with-meta (~'fn ~'[ctx & [class]] ~expr)
                 {:expr-str ~(slow-pprint-str expr)})))
