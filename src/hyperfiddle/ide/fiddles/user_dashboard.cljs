4d7e26f5844b3c8c195a8ade79c46318e8896752
Dustin Getz <dustin.getz@gmail.com>
2018-04-20 12:45:31 -0400

    fix user's domains renderer (username dropdown) #208

--- src/hyperfiddle/ide/fiddles/user_dashboard.cljs
+++ src/hyperfiddle/ide/fiddles/user_dashboard.cljs
@@ -19,14 +19,11 @@
       ; todo https://auth0.com/docs/logout
       [:span.nav-link.auth {:key :logout}
        [:a {:href "/logout"} "logout"]]])
-   [markdown (r/cursor (:hypercrud.browser/fiddle ctx) [:db/doc])]
+   [markdown @(r/cursor (:hypercrud.browser/fiddle ctx) [:db/doc])]
    [:ul.link-list
     (->> @(:hypercrud.browser/result ctx)
          (sort-by :domain/ident)
          (map (fn [domain]
                 [:li {:key (hash (:db/id domain))}
                  [:a {:href (str "http://" (:domain/ident domain) "." (:hyperfiddle-hostname ctx) "/")} (:domain/ident domain)]]))
-         (doall))]
-   [:style {:dangerouslySetInnerHTML {:__html "
-.hyperfiddle-user-dashboard { width: 250px; }
-"}}]])
+         (doall))]])
