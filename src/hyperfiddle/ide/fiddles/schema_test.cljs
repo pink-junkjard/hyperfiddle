8dfc006092bcf093533d4ba502d4402bb9c553be
Dustin Getz <dustin.getz@gmail.com>
2018-04-20 12:36:17 -0400

    move schema test next to schema and make .cljs

---
+++ src/hyperfiddle/ide/fiddles/schema_test.cljs
@@ -0,0 +1,35 @@
+(ns hyperfiddle.ide.fiddles.schema_test
+  (:require [clojure.test :refer [deftest is]]
+            [contrib.reactive :as r]
+            [contrib.string :refer [slow-pprint-str]]
+            [hypercrud.browser.browser-ui-test :refer [test-renderer-str]]
+            [hyperfiddle.ide.fiddles.schema :as schema]))
+
+
+(deftest schema-renderer []
+  (let [ctx {:hypercrud.browser/attribute (r/atom nil)
+             :hypercrud.browser/fiddle (r/atom nil)
+             :hypercrud.browser/find-element (r/atom nil)
+             :hypercrud.browser/links (r/atom nil)
+             :hypercrud.browser/ordered-fes (r/atom [])
+             :hypercrud.browser/request (r/atom nil)
+             :hypercrud.browser/result (r/atom [])
+             :relations (r/atom [])
+             :hypercrud.browser/schema (r/atom nil)
+             :hypercrud.browser/schemas (r/atom nil)}]
+    ; just test it renderers something
+    (is (not (nil? (test-renderer-str (:fiddle/renderer (schema/schema "$")) ctx))))))
+
+(deftest db-attribute-renderer []
+  (let [ctx {:hypercrud.browser/attribute (r/atom nil)
+             :hypercrud.browser/fiddle (r/atom nil)
+             :hypercrud.browser/find-element (r/atom nil)
+             :hypercrud.browser/links (r/atom nil)
+             :hypercrud.browser/ordered-fes (r/atom [])
+             :hypercrud.browser/request (r/atom nil)
+             :hypercrud.browser/result (r/atom nil)
+             :relation (r/atom nil)
+             :hypercrud.browser/schema (r/atom nil)
+             :hypercrud.browser/schemas (r/atom nil)}]
+    ; just test it renderers something
+    (is (not (nil? (test-renderer-str (:fiddle/renderer (schema/db-attribute-edit "$")) ctx))))))
