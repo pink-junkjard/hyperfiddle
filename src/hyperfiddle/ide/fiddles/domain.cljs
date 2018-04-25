5e92022f0a32ec955017a5930c4275d50dafc4bf
Dustin Getz <dustin.getz@gmail.com>
2018-04-25 18:54:58 -0400

    whitespace noop

--- src/hyperfiddle/ide/fiddles/domain.cljs
+++ src/hyperfiddle/ide/fiddles/domain.cljs
@@ -1,6 +1,7 @@
 (ns hyperfiddle.ide.fiddles.domain
   (:require [cats.core :as cats]
             [cats.monad.either :as either]
+            [contrib.data :refer [unwrap]]
             [clojure.string :as string]
             [contrib.string :refer [safe-read-edn-string]]
             [hypercrud.types.URI :refer [is-uri?]]))
@@ -14,11 +15,7 @@
 (defn ^:export domain-environment-renderer [field props ctx]
   [:div
    [hypercrud.ui.attribute.code/code field props ctx]
-   (->> (-> @(:value ctx)
-            (safe-read-edn-string)                          ; memoized?
-            ; todo something with this error
-            (cats/mplus (either/right nil))
-            (cats/extract))
+   (->> (unwrap (safe-read-edn-string @(:value ctx))) ; memoized? todo something with this error
         (filter (fn [[k v]] (and (string? k) (string/starts-with? k "$") (is-uri? v))))
         (map (fn [[$db _]]
                (let [props {:route [(keyword "hyperfiddle.schema" $db)]}]
--- src/hyperfiddle/ide/fiddles/not_found.cljc
+++ src/hyperfiddle/ide/fiddles/not_found.cljc
@@ -1,9 +1,7 @@
-(ns hyperfiddle.ide.fiddles.not-found
-  (:require [contrib.string :refer [mpprint-str pprint-str]]))
+(ns hyperfiddle.ide.fiddles.not-found)
 
 
 (def not-found
   {:fiddle/ident :hyperfiddle.system/not-found
    :fiddle/type :blank
    :fiddle/markdown "# Fiddle not found"})
-
