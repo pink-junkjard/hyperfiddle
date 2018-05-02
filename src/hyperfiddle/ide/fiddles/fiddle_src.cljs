8bbe4e4a4fae5f7304db306b62948c1560d282e8
Dustin Getz <dustin.getz@gmail.com>
2018-05-02 17:38:42 -0400

    fiddle src doc tweaks

--- src/hyperfiddle/ide/fiddles/fiddle_src.cljs
+++ src/hyperfiddle/ide/fiddles/fiddle_src.cljs
@@ -11,7 +11,7 @@
   (let [ctx-real (dissoc ctx-real :user-renderer)           ; this needs to not escape this level; inline links can't ever get it
         ctx' (shadow-fiddle ctx-real)
         rtype (:fiddle/type @(:hypercrud.browser/result ctx'))]
-    [:div {:class class}
+    [:div {:class (str class " fiddle-src")}
      [:h3 "fiddle src"]
      ((:cell ctx') [true 0 :fiddle/ident] ctx')
      ((:cell ctx') [true 0 :fiddle/type] ctx')
@@ -30,14 +30,25 @@
      ((:browse ctx-real) :attribute-renderers [] ctx-real)
      ]))
 
+(defn schema-links [ctx]
+  (->> (-> ctx :target-domain :domain/environment)
+       (filter (fn [[k v]] (and (str/starts-with? k "$") (is-uri? v))))
+       sort
+       (map (fn [[$db _]]
+              (let [props {:route [(keyword "hyperfiddle.schema" $db)]}]
+                ^{:key $db}
+                [(:navigate-cmp ctx) props $db])))
+       (doall)))
+
 
 (defn ^:export fiddle-query-renderer [field props ctx]
   [:div
    [hypercrud.ui.attribute.code/code field props ctx]
-   (->> (-> ctx :target-domain :domain/environment)
-        (filter (fn [[k v]] (and (str/starts-with? k "$") (is-uri? v))))
-        (map (fn [[$db _]]
-               (let [props {:route [(keyword "hyperfiddle.schema" $db)]}]
-                 ^{:key $db}
-                 [(:navigate-cmp ctx) props (str $db " schema")])))
-        (doall))])
+   "schema: " (schema-links ctx)
+   [markdown "See [example queries]() and the [Datomic query docs](https://docs.datomic.com/cloud/query/query-data-reference.html)"]])
+
+(defn ^:export fiddle-pull [field props ctx]
+  [:div
+   [hypercrud.ui.attribute.code/code field props ctx]
+   "schema: " (schema-links ctx)
+   [markdown "See [Datomic pull docs](https://docs.datomic.com/on-prem/query.html)"]])
