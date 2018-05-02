a78b444fae398133fc71c6835236a15d0aca1e38
Dustin Getz <dustin.getz@gmail.com>
2018-05-02 18:23:20 -0400

    remove domain-environment-renderer, schema is in fiddle-src now

--- src/hyperfiddle/ide/fiddles/domain.cljs
+++ src/hyperfiddle/ide/fiddles/domain.cljs
@@ -11,14 +11,3 @@
   (let [ident @(contrib.reactive/cursor (:cell-data ctx) [:domain/ident])
         href (str "http://" ident "." (:hyperfiddle-hostname ctx))]
     [:a {:href href} href]))
-
-(defn ^:export domain-environment-renderer [field props ctx]
-  [:div
-   [hypercrud.ui.attribute.code/code field props ctx]
-   (->> (unwrap (safe-read-edn-string @(:value ctx))) ; memoized? todo something with this error
-        (filter (fn [[k v]] (and (string? k) (string/starts-with? k "$") (is-uri? v))))
-        (map (fn [[$db _]]
-               (let [props {:route [(keyword "hyperfiddle.schema" $db)]}]
-                 ^{:key $db}
-                 [(:navigate-cmp ctx) props (str $db " schema")])))
-        (doall))])
