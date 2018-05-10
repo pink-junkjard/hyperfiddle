13bf13d1bb719084a91b289c09880675cdb6673d
Dustin Getz <dustin.getz@gmail.com>
2018-05-10 16:58:41 -0400

    Fix reactions in fiddle-src

--- src/hyperfiddle/ide/fiddles/fiddle_src.cljs
+++ src/hyperfiddle/ide/fiddles/fiddle_src.cljs
@@ -40,46 +40,53 @@
    :fiddle/renderer "See [:fiddle/renderer examples](http://www.hyperfiddle.net/:docs!fiddle-renderer/)."
    :fiddle/links "See [:fiddle/links examples](http://www.hyperfiddle.net/:docs!fiddle-links/)."})
 
-(defn control [k ctx]
-  (get
-    {:fiddle/pull (r/partial cell-wrap (r/partial control-with-unders (fragment :_ [:span.schema "schema: " (schema-links ctx)] [markdown (:fiddle/pull underdocs)])))
-     :fiddle/query (r/partial cell-wrap (r/partial control-with-unders (fragment :_ [:span.schema "schema: " (schema-links ctx)] [markdown (:fiddle/query underdocs)])))
-     :fiddle/markdown (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/markdown underdocs)]))
-     :fiddle/css (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/css underdocs)]))
-     :fiddle/renderer (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/renderer underdocs)]))
-     :fiddle/links (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/links underdocs)]))
-     }
-    k))
-
 (defn fiddle-src-renderer [ctx-real class & {:keys [embed-mode]}]
   (let [ctx-real (dissoc ctx-real :user-renderer)           ; this needs to not escape this level; inline links can't ever get it
         ctx (shadow-fiddle ctx-real)
-        {:keys [:fiddle/type :fiddle/ident]} @(:hypercrud.browser/result ctx)]
-    [:div.fiddle-src {:class class}
-     [:h3 (str ident) " source"]
-     ((:cell ctx) [true 0 :fiddle/ident] ctx)
-     ((:cell ctx) [true 0 :fiddle/type] ctx)
-     (case type
-       :entity ((:cell ctx) [true 0 :fiddle/pull] ctx (control :fiddle/pull ctx))
-       :query ((:cell ctx) [true 0 :fiddle/query] ctx (control :fiddle/query ctx))
-       :blank nil
-       nil nil)
-     ((:cell ctx) [true 0 :fiddle/markdown] ctx (control :fiddle/markdown ctx))
-     ((:cell ctx) [true 0 :fiddle/css] ctx (control :fiddle/css ctx))
-     ((:cell ctx) [true 0 :fiddle/renderer] ctx (control :fiddle/renderer ctx))
-     (when-not embed-mode ((:cell ctx) [true 0 :fiddle/links] ctx-real (control :fiddle/links ctx)))
-     ((:cell ctx) [true 0 :fiddle/entrypoint?] ctx)
-     (when-not embed-mode ((:anchor ctx) :hyperfiddle/remove [0] ctx "Remove fiddle"))
-     (when-not embed-mode ((:browse ctx-real) :attribute-renderers [] ctx-real))
-     ]))
+        {:keys [:fiddle/type :fiddle/ident]} @(:hypercrud.browser/result ctx)
+        controls
+        {:fiddle/pull (r/partial cell-wrap (r/partial control-with-unders (fragment :_ [:span.schema "schema: " (schema-links ctx)] [markdown (:fiddle/pull underdocs)])))
+         :fiddle/query (r/partial cell-wrap (r/partial control-with-unders (fragment :_ [:span.schema "schema: " (schema-links ctx)] [markdown (:fiddle/query underdocs)])))
+         :fiddle/markdown (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/markdown underdocs)]))
+         :fiddle/css (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/css underdocs)]))
+         :fiddle/renderer (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/renderer underdocs)]))
+         :fiddle/links (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/links underdocs)]))
+         }]
+    (fn [ctx-real class & {:keys [embed-mode]}]
+      [:div.fiddle-src {:class class}
+       [:h3 (str ident) " source"]
+       ((:cell ctx) [true 0 :fiddle/ident] ctx)
+       ((:cell ctx) [true 0 :fiddle/type] ctx)
+       (case type
+         :entity ((:cell ctx) [true 0 :fiddle/pull] ctx (controls :fiddle/pull))
+         :query ((:cell ctx) [true 0 :fiddle/query] ctx (controls :fiddle/query))
+         :blank nil
+         nil nil)
+       ((:cell ctx) [true 0 :fiddle/markdown] ctx (controls :fiddle/markdown))
+       ((:cell ctx) [true 0 :fiddle/css] ctx (controls :fiddle/css))
+       ((:cell ctx) [true 0 :fiddle/renderer] ctx (controls :fiddle/renderer))
+       (when-not embed-mode ((:cell ctx) [true 0 :fiddle/links] ctx-real (controls :fiddle/links)))
+       ((:cell ctx) [true 0 :fiddle/entrypoint?] ctx)
+       (when-not embed-mode ((:anchor ctx) :hyperfiddle/remove [0] ctx "Remove fiddle"))
+       (when-not embed-mode ((:browse ctx-real) :attribute-renderers [] ctx-real))
+       ])))
 
 (defn docs-embed [& attrs]
   (fn [ctx-real class & {:keys [embed-mode]}]
     (let [ctx-real (dissoc ctx-real :user-renderer)         ; this needs to not escape this level; inline links can't ever get it
           ctx (shadow-fiddle ctx-real)
-          {:keys [:fiddle/ident]} @(:hypercrud.browser/result ctx)]
-      (into
-        [:div.fiddle-src {:class class}
-         #_[:h3 (str ident) " source"]]
-        (for [k attrs]
-          ((:cell ctx) [true 0 k] ctx (control k ctx)))))))
+          {:keys [:fiddle/ident]} @(:hypercrud.browser/result ctx)
+          controls
+          {:fiddle/pull (r/partial cell-wrap (r/partial control-with-unders (fragment :_ [:span.schema "schema: " (schema-links ctx)] [markdown (:fiddle/pull underdocs)])))
+           :fiddle/query (r/partial cell-wrap (r/partial control-with-unders (fragment :_ [:span.schema "schema: " (schema-links ctx)] [markdown (:fiddle/query underdocs)])))
+           :fiddle/markdown (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/markdown underdocs)]))
+           :fiddle/css (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/css underdocs)]))
+           :fiddle/renderer (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/renderer underdocs)]))
+           :fiddle/links (r/partial cell-wrap (r/partial control-with-unders [markdown (:fiddle/links underdocs)]))
+           }]
+      (fn [ctx-real class & {:keys [embed-mode]}]
+        (into
+          [:div.fiddle-src {:class class}
+           #_[:h3 (str ident) " source"]]
+          (for [k attrs]
+            ((:cell ctx) [true 0 k] ctx (controls k))))))))
