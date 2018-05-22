86edbd7a185942bedf543555e134db7debcb2d47
Dustin Getz <dustin.getz@gmail.com>
2018-05-22 18:52:06 -0400

    #277 fix domain panel

--- src/contrib/reagent.cljs
+++ src/contrib/reagent.cljs
@@ -5,10 +5,9 @@
 (defn fragment [react-key & xs]
   (js/reactCreateFragment (clj->js {react-key (map reagent/as-element xs)})))
 
-(defn dress "wrap naked strings into divs" [C ?f]
+(defn dress "wrap naked strings into divs" [C f]
   (fn [value]
-    (if ?f
-      (let [hiccup-template (?f value)]
-        (if (vector? hiccup-template)
-          hiccup-template
-          [C hiccup-template])))))
+    (let [hiccup-template (f value)]
+      (if (vector? hiccup-template)
+        hiccup-template
+        [C hiccup-template]))))
--- src/hyperfiddle/ui/markdown_extensions.cljs
+++ src/hyperfiddle/ui/markdown_extensions.cljs
@@ -35,7 +35,7 @@
   (let [?f (read-eval-with-bindings content)
         kwargs (flatten (seq (keywordize-keys props)))
         path (into [true] (unwrap (memoized-safe-read-edn-string (str "[" argument "]"))))]
-    (apply (:cell ctx) path ctx (dress :div (fix-arity ?f 1)) kwargs)))
+    (apply (:cell ctx) path ctx (if ?f (dress :div (fix-arity ?f 1))) kwargs)))
 
 (defn ^:deprecated -table [content argument {:keys [class] :as props} ctx]
   [:div.unp (hypercrud.ui.table/Table ctx)])
