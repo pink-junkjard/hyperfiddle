1d73b9bb875c1e19dc20575b5bf1d3197c7aabe5
Dustin Getz <dustin.getz@gmail.com>
2018-05-27 12:38:09 -0400

    Better default fiddle/renderer

--- src/hypercrud/ui/result.clj
+++ src/hypercrud/ui/result.clj
@@ -5,19 +5,21 @@
 
 
 (def expr
-  '[:div {:class class}
-    (let [fiddle @(:hypercrud.browser/fiddle ctx)]
-      [hyperfiddle.ui/markdown (:fiddle/markdown fiddle) ctx])])
+  '(let [{:keys [:hypercrud.browser/fiddle
+                 :hypercrud.browser/result]} ctx]
+     [:div {:class class}
+      [hyperfiddle.ui/markdown (:fiddle/markdown @fiddle) ctx]]))
 
 (def expr-manually-formatted
-  ; Format this manually because:
+  ; Format this manually:
   ; - Syntax quote will expand @ into `(clojure.core/deref ..)`
   ; - pretty printers suck at clojure, even the slow one
   ; embedded newline lets this pass the cursive clojure formatter
   "
-[:div {:class class}
- (let [fiddle @(:hypercrud.browser/fiddle ctx)]
-   [hyperfiddle.ui/markdown (:fiddle/markdown fiddle) ctx])]")
+(let [{:keys [:hypercrud.browser/fiddle
+              :hypercrud.browser/result]} ctx]
+  [:div {:class class}
+   [hyperfiddle.ui/markdown (:fiddle/markdown @fiddle) ctx]])")
 
 (defmacro -build-fiddle []                                  ; Pretty print at compile-time
   `(with-meta (~'fn ~'[ctx & [class]] ~expr)
