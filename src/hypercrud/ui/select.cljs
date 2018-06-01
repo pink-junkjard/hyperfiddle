a0444f3d3df3cb707b33a69b93169b353cafa483
Karl Hardenstine <khardenstine@gmail.com>
2018-06-01 12:41:24 -0400

    Select perf fixes; select option-props; allow alt clicks on read-only selects #99

--- src/hypercrud/ui/select.cljs
+++ src/hypercrud/ui/select.cljs
@@ -36,22 +36,22 @@
     (either/branch label' pr-str identity)))
 
 (defn select-boolean* [value props ctx]
-  (let [props {;; normalize value for the dom - value is either nil, an :ident (keyword), or eid
+  (let [option-props {:disabled (or (boolean (:read-only props))
+                                    (nil? @(r/cursor (:cell-data ctx) [:db/id])))}
+        props {;; normalize value for the dom - value is either nil, an :ident (keyword), or eid
                :value (if (nil? value) "" (str value))
                ;; reconstruct the typed value
                :on-change #(let [v (case (.-target.value %)
                                      "" nil
                                      "true" true
                                      "false" false)]
-                             ((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) v)))
-               :disabled (or (boolean (:read-only props))
-                             (nil? @(r/cursor (:cell-data ctx) [:db/id])))}]
+                             ((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) v)))}]
     [:select (dissoc props :label-fn)
-     [:option {:key true :value "true"} "True"]
-     [:option {:key false :value "false"} "False"]
-     [:option {:key :nil :value ""} "--"]]))
+     [:option (assoc option-props :key true :value "true") "True"]
+     [:option (assoc option-props :key false :value "false") "False"]
+     [:option (assoc option-props :key :nil :value "") "--"]]))
 
-(defn select-anchor-renderer' [ctx props]
+(defn select-anchor-renderer' [props option-props ctx]
   ; hack in the selected value if we don't have options hydrated?
   ; Can't, since we only have the #DbId hydrated, and it gets complicated with relaton vs entity etc
   (let [no-options? (empty? @(:relations ctx))
@@ -71,18 +71,18 @@
             (mapv (juxt (comp :db/id first) #(label-fn % ctx)))
             (sort-by second)
             (map (fn [[id label]]
-                   [:option {:key (str id) :value id} label])))
+                   [:option (assoc option-props :key (str id) :value id) label])))
        [:option {:key :blank :value ""} "--"])]))
 
 (defn select-error-cmp [msg]
   [:span msg])
 
-(defn select-anchor-renderer [props ctx-options]
-  (case @(r/cursor (:hypercrud.browser/fiddle ctx-options) [:fiddle/type])
+(defn select-anchor-renderer [props option-props ctx]
+  (case @(r/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/type])
     :entity [select-error-cmp "Only fiddle type `query` is supported for select options"]
     :blank [select-error-cmp "Only fiddle type `query` is supported for select options"]
-    :query (if (:relations ctx-options)
-             [select-anchor-renderer' ctx-options props]
+    :query (if (:relations ctx)
+             [select-anchor-renderer' props option-props ctx]
              [select-error-cmp "Tuples and scalars are unsupported for select options. Please fix your options query to return a relation or collection"])
     ; default
     [select-error-cmp "Only fiddle type `query` is supported for select options"]))
@@ -90,20 +90,22 @@
 (def always-user (atom :user))
 
 (let [on-change (fn [ctx id]
-                  ((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) id)))]
+                  ((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) id)))
+      select-value (fn [value]
+                     (if (nil? value)
+                       ""
+                       (str (:db/id value))))]
   (defn select* [options-link ctx props]
     (either/branch
       (link/eval-hc-props (:hypercrud/props options-link) ctx)
       (fn [e] [(ui-error/error-comp ctx) e])
       (fn [hc-props]
-        (let [props (merge hc-props
-                           ; normalize value for the dom - nil, kw or eid
-                           {:value (if (nil? @(:value ctx))
-                                     ""
-                                     (str (:db/id @(:value ctx))))
-                            :on-change (r/partial on-change ctx) ; reconstruct the typed value
-                            :disabled (or (boolean (:read-only props))
-                                          (nil? @(r/cursor (:cell-data ctx) [:db/id])))})]
+        (let [option-props {:disabled (or (boolean (:read-only props))
+                                          (nil? @(r/cursor (:cell-data ctx) [:db/id])))}
+              props (assoc hc-props
+                      :on-change (r/partial on-change ctx)  ;reconstruct the typed value
+                      ; normalize value for the dom - nil, kw or eid
+                      :value @(r/fmap select-value (:value ctx)))]
           [browser/ui options-link (assoc ctx
                                      :hypercrud.ui/display-mode always-user
-                                     :user-renderer (r/partial select-anchor-renderer props))])))))
+                                     :user-renderer (r/partial select-anchor-renderer props option-props))])))))
