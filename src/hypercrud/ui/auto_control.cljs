ce0965b85e3bc0ccf72fbac79cdda3176a9bec30
Dustin Getz <dustin.getz@gmail.com>
2018-06-10 15:17:05 -0400

    #314 fix component, and cleanup

--- src/hypercrud/ui/auto_control.cljs
+++ src/hypercrud/ui/auto_control.cljs
@@ -1,10 +1,10 @@
 (ns hypercrud.ui.auto-control
   (:require
     [contrib.reactive :as r]
-    [clojure.core.match :refer [match match*]]
-    [hypercrud.ui.attribute.edn :as edn]
-    [hypercrud.ui.attribute.instant :as instant]
     [contrib.string :refer [blank->nil]]
+    [clojure.core.match :refer [match*]]
+    [hypercrud.ui.attribute.edn :refer [edn edn-many]]
+    [hypercrud.ui.attribute.instant :refer [instant]]
     [hypercrud.ui.safe-render :refer [portal-markup]]
     [hypercrud.ui.table-cell :as table-cell]
     [hypercrud.ui.util :refer [attr-renderer]]
@@ -31,26 +31,26 @@
           [:table :keyword :one _] widget/keyword
           [:table :string :one _] widget/string
           [:table :long :one _] widget/long
-          [:table :instant :one _] instant/instant
-          [:table :ref :one true] table-cell/ref-one-component
-          [:table :ref :one false] widget/ref
-          [:table :ref :many true] table-cell/ref-many
-          [:table :ref :many false] edn/edn-many
+          [:table :instant :one _] instant
+          [:table :ref :one :component] table-cell/ref-one-component
+          [:table :ref :one nil] widget/ref
+          [:table :ref :many :component] table-cell/ref-many
+          [:table :ref :many nil] edn-many
 
           ; :block vs :inline-block currently handled inside the widget, todo
           [_ :boolean :one _] widget/boolean
           [_ :keyword :one _] widget/keyword
           [_ :string :one _] widget/string
           [_ :long :one _] widget/long
-          [_ :instant :one _] instant/instant
-          [_ :ref :one true] widget/ref-component
-          [_ :ref :one false] widget/ref
-          [_ :ref :many true] widget/ref-many-table
-          [_ :ref :many false] edn/edn-many
+          [_ :instant :one _] instant
+          [_ :ref :one :component] widget/ref-component
+          [_ :ref :one nil] widget/ref
+          [_ :ref :many :component] widget/ref-many-table
+          [_ :ref :many nil] edn-many
 
           ; Unmatched valueType, like #uri and #uuid
-          [_ _ :many _] edn/edn-many
-          [_ _ :one _] edn/edn
+          [_ _ :many _] edn-many
+          [_ _ :one _] edn
 
           [_ nil _ _] widget/text                           ; something is wrong
           )))))
