ed656b2b476ec474c4793072b5a79134e013ae5a
Dustin Getz <dustin.getz@gmail.com>
2018-06-12 16:28:59 -0400

    Kill widget wrapper div

--- src/hyperfiddle/ui/controls.cljs
+++ src/hyperfiddle/ui/controls.cljs
@@ -11,34 +11,30 @@
 
 
 (defn keyword [value ctx props]
-  [:div
-   (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
-         on-change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) %))]
-     [input/keyword-input* value on-change! props])])
+  (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
+        on-change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) %))]
+    [input/keyword-input* value on-change! props]))
 
 (defn string [value ctx props]
-  [:div
-   (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
-         on-change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) (empty->nil %)))]
-     [input/input* value on-change! props])])
+  (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
+        on-change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) (empty->nil %)))]
+    [input/input* value on-change! props]))
 
 (defn long [value ctx props]
   (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
         on-change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) %))]
-    [:div
-     [input/validated-input
-      value on-change!
-      #(js/parseInt % 10) (fnil str "")
-      #(or #_(= "" %) (integer? (js/parseInt % 10)))
-      props]]))
+    [input/validated-input
+     value on-change!
+     #(js/parseInt % 10) (fnil str "")
+     #(or #_(= "" %) (integer? (js/parseInt % 10)))
+     props]))
 
 (defn ^:export boolean [value ctx props]
-  [:div
-   (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
-         change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx)
-                                                            @(:hypercrud.browser/fat-attribute ctx)
-                                                            (not value)))]
-     (contrib.ui/checkbox value change! props))])
+  (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
+        change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx)
+                                                           @(:hypercrud.browser/fat-attribute ctx)
+                                                           (not value)))]
+    (contrib.ui/checkbox value change! props)))
 
 (defn select-boolean* [value props ctx]
   (let [option-props {:disabled (or (cljs.core/boolean (:read-only props))
@@ -57,23 +53,18 @@
      [:option (assoc option-props :key :nil :value "") "--"]]))
 
 (defn ^:export tristate-boolean [value ctx props]
-  [:div
-   (select-boolean* value props ctx)])
-
-(defn id* [value ctx props]
-  (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
-        on-change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) %))]
-    (input/id-input value on-change! props)))
+  (select-boolean* value props ctx))
 
 (defn dbid [value ctx props]
-  [:div (id* value ctx props)])
+  (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
+        on-change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) %))]
+    [input/id-input value on-change! props]))
 
 (defn text [value ctx props]
-  [:div
-   [:span
-    (case @(r/cursor (:hypercrud.browser/fat-attribute ctx) [:db/cardinality :db/ident])
-      :db.cardinality/many (map pr-str value)
-      (pr-str value))]])
+  [:span
+   (case @(r/cursor (:hypercrud.browser/fat-attribute ctx) [:db/cardinality :db/ident])
+     :db.cardinality/many (map pr-str value)
+     (pr-str value))])
 
 ;/*.value span.text {*/
 ;    /*margin: 3px 0;*/
@@ -85,37 +76,34 @@
 ;/*}*/
 
 (defn ^:export instant [value ctx props]
-  [:div
-   (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
-         change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) %))
-         widget (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
-                  :hyperfiddle.ui.layout/block recom-date
-                  :hyperfiddle.ui.layout/table recom-date)]
-     [widget value change! props])])
+  (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
+        change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) %))
+        widget (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
+                 :hyperfiddle.ui.layout/block recom-date
+                 :hyperfiddle.ui.layout/table recom-date)]
+    [widget value change! props]))
 
 (defn ^:export code [value ctx props]
   (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
         change! (fn [%]
                   (let [tx (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) (empty->nil %))]
                     ((:user-with! ctx) tx)))]
-    [:div
-     (let [control (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
-                     :hyperfiddle.ui.layout/block code-block
-                     :hyperfiddle.ui.layout/table code-inline-block)]
-       ; backwards args - props last
-       [control props value change!])]))
+    (let [control (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
+                    :hyperfiddle.ui.layout/block code-block
+                    :hyperfiddle.ui.layout/table code-inline-block)]
+      ; backwards args - props last
+      [control props value change!])))
 
 (defn ^:export markdown-editor [value ctx props]
   (let [path [(:fe-pos ctx) (:hypercrud.browser/attribute ctx)]
         props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
         change! #((:user-with! ctx) (tx/update-entity-attr @(:cell-data ctx) @(:hypercrud.browser/fat-attribute ctx) (empty->nil %)))]
-    [:div
-     (let [widget (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
-                    :hyperfiddle.ui.layout/block code-block
-                    :hyperfiddle.ui.layout/table code-inline-block)
-           props (assoc props :mode "markdown" :lineWrapping true)]
-       ; backwards args - props last
-       [widget props value change!])]))
+    (let [widget (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
+                   :hyperfiddle.ui.layout/block code-block
+                   :hyperfiddle.ui.layout/table code-inline-block)
+          props (assoc props :mode "markdown" :lineWrapping true)]
+      ; backwards args - props last
+      [widget props value change!])))
 
 (defn ^:export edn-many [value ctx props]
   (let [valueType @(r/cursor (:hypercrud.browser/fat-attribute ctx) [:db/valueType :db/ident])
@@ -134,8 +122,7 @@
         widget (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
                  :hyperfiddle.ui.layout/block edn-block
                  :hyperfiddle.ui.layout/table edn-inline-block)]
-    [:div
-     [widget value change! props]]))
+    [widget value change! props]))
 
 (defn ^:export edn [value ctx props]
   (let [props (update props :read-only #(or % (nil? @(r/cursor (:cell-data ctx) [:db/id]))))
@@ -143,8 +130,7 @@
         widget (case (:hyperfiddle.ui/layout ctx :hyperfiddle.ui.layout/block)
                  :hyperfiddle.ui.layout/table edn-inline-block
                  :hyperfiddle.ui.layout/block edn-block)]
-    [:div
-     [widget value change! props]]))
+    [widget value change! props]))
 
 (defn textarea* [{:keys [value on-change] :as props}]       ; unused
   (let [on-change #(let [newval (.. % -target -value)]
