8f3bc572ce255039c95a585e2546ade9885a39f9
Dustin Getz <dustin.getz@gmail.com>
2018-06-20 17:51:01 -0400

    remove comment

--- src/hyperfiddle/ui/form.cljs
+++ src/hyperfiddle/ui/form.cljs
@@ -59,7 +59,7 @@
 (defn form-field "Form fields are label AND value. Table fields are label OR value."
   [hyper-control ?f ctx props]                              ; fiddle-src wants to fallback by passing nil here explicitly
   (assert @(:hypercrud.ui/display-mode ctx))
-  (let [state (r/atom {::magic-new-a nil})]                 ; ^{:key (hash @(r/fmap keys (context/entity ctx)))}
+  (let [state (r/atom {::magic-new-a nil})]
     (fn [hyper-control ?f ctx props]
       (let [ctx (assoc ctx :hyperfiddle.ui.form/state state)]
         (ui-block-border-wrap
