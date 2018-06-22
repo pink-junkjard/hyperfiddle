51fd3bc5e8c7e6acb6becf7fb80dac3b1438fe4e
Dustin Getz <dustin.getz@gmail.com>
2018-06-22 11:07:37 -0400

    hflive col order - Bootstrap 4 #327

--- src/hyperfiddle/ide/fiddles/fiddle_src.cljs
+++ src/hyperfiddle/ide/fiddles/fiddle_src.cljs
@@ -102,14 +102,13 @@
   (let [state (r/atom {:edn-fiddle false :edn-result false})]
     (fn [rel ctx & fiddle-attrs]
       [:div.row.hf-live.unp
-       ; Reverse order so it looks right on mobile, larger views reorder
        (let [as-edn (r/cursor state [:edn-result])]
-         [:div.col-sm-6.col-sm-push-6
+         [:div.col-sm.order-sm-2.order-xs-1
           [:div "Result:" [contrib.ui/easy-checkbox-boolean " EDN?" as-edn {:class "hf-live"}]]
           (browse rel [] ctx (if @as-edn (r/partial result-edn [])))])
        (let [as-edn (r/cursor state [:edn-fiddle])
              f (r/partial (if @as-edn result-edn docs-embed) fiddle-attrs)]
-         [:div.col-sm-6.col-sm-pull-6
+         [:div.col-sm.order-sm-1.order-xs-2
           [:div "Interactive Hyperfiddle editor:" [contrib.ui/easy-checkbox-boolean " EDN?" as-edn {:class "hf-live"}]]
           (browse rel [] ctx f {:frag ":src" :class "devsrc"})])
        ])))
