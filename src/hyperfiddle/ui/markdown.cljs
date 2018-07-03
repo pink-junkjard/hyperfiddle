4cf518702503eb2a2a5ba5f65fd28157dbf52dbb
Dustin Getz <dustin.getz@gmail.com>
2018-07-03 15:31:35 -0400

    Fix vanilla markdown anchors, broken by !a

--- src/hyperfiddle/ui/markdown.cljs
+++ src/hyperfiddle/ui/markdown.cljs
@@ -10,4 +10,6 @@
 
 
 (defn a [content argument props ctx]
-  [:a (merge {:href argument} (dissoc props :children)) content])
+  [:a (merge {:href argument} (dissoc props :children))
+   ; backwards compat with vanilla markdown anchors
+   (or (:children props) content)])
