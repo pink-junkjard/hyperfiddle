2d0f94a712de760290794e6cfdc6b5729ea67cf0
Karl Hardenstine <khardenstine@gmail.com>
2018-08-08 23:01:32 -0400

    Fix hf-live sys fiddle renderer

--- resources/ide/hf_live_renderer.cljs
+++ resources/ide/hf_live_renderer.cljs
@@ -1,5 +1,5 @@
 [:div {:class class}
  (let [[_ [inner-fiddle & inner-args]] (:route ctx)]
-   [hyperfiddle.ui/iframe
+   [hyperfiddle.ui/iframe ctx
     {:route [inner-fiddle inner-args]
      :user-renderer (hyperfiddle.ide.hf-live/hf-live)}])]
