be89aed03acc97906253758c70291c5d001050aa
Karl Hardenstine <khardenstine@gmail.com>
2018-08-08 20:23:27 -0400

    Fix auto-transact and stage area
    hyperfiddle/hyperfiddle#431
    hyperfiddle/hyperfiddle#436

--- src/hyperfiddle/service/node/ssr.cljs
+++ src/hyperfiddle/service/node/ssr.cljs
@@ -152,8 +152,8 @@
     (-> (foundation/bootstrap-data rt foundation/LEVEL-NONE load-level (.-path req) (::runtime/global-basis initial-state))
         (p/then (fn []
                   (let [auto-transact (->> @(runtime/state rt [::runtime/domain :domain/databases])
-                                           (map (juxt :domain.database/name (fn [{:keys [:domain.database/record]}]
-                                                                              (security/attempt-to-transact? record @(runtime/state rt [::runtime/user-id])))))
+                                           (map :domain.database/record)
+                                           (map (juxt :database/uri #(security/attempt-to-transact? % @(runtime/state rt [::runtime/user-id]))))
                                            (into {}))]
                     (runtime/dispatch! rt [:set-auto-transact auto-transact]))))
         (p/then (constantly 200))
