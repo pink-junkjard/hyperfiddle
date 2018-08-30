b3cf27ca8898304db0673fed2530ed71360bb240
Dustin Getz <dustin.getz@gmail.com>
2018-08-30 18:22:28 -0400

    Regression: table sorting #447

--- src/hyperfiddle/ui/sort.cljs
+++ src/hyperfiddle/ui/sort.cljs
@@ -5,38 +5,31 @@
             [hypercrud.browser.link :as link]))
 
 
-; sorting currently breaks click handling in popovers
-(defn links-dont-break-sorting? [ctx]
-  (->> @(:hypercrud.browser/links ctx)
-       ;(filter (partial link/draw-link? (:hypercrud.browser/path ctx)))
-       (not-any? link/popover-link?)))
-
 (defn sortable? [ctx]
   (let [?dbname (context/dbname ctx)
         ?last-segment (last (:hypercrud.browser/path ctx))]
-    (and
-      (if (and ?dbname (context/attribute-segment? ?last-segment)) ; [fe attr] or [attr], NOT [fe] or []
-        (let [{:keys [:db/cardinality :db/valueType]} @(r/cursor (:hypercrud.browser/schemas ctx) [?dbname ?last-segment])]
-          (and
-            (= (:db/ident cardinality) :db.cardinality/one)
-            ; ref requires more work (inspect label-prop)
-            (contains? #{:db.type/keyword
-                         :db.type/string
-                         :db.type/boolean
-                         :db.type/long
-                         :db.type/bigint
-                         :db.type/float
-                         :db.type/double
-                         :db.type/bigdec
-                         :db.type/instant
-                         :db.type/uuid
-                         :db.type/uri
-                         :db.type/bytes
-                         :db.type/code}
-                       (:db/ident valueType))))
-        ; [fe] when aggregates or variables
-        (and (not ?dbname) ?last-segment))
-      @(r/track links-dont-break-sorting? ctx))))
+    ; Used to check links dont break sorting, but those cases don't happen anymore.
+    (if (and ?dbname (context/attribute-segment? ?last-segment)) ; [fe attr] or [attr], NOT [fe] or []
+      (let [{:keys [:db/cardinality :db/valueType]} @(r/cursor (:hypercrud.browser/schemas ctx) [?dbname ?last-segment])]
+        (and
+          (= (:db/ident cardinality) :db.cardinality/one)
+          ; ref requires more work (inspect label-prop)
+          (contains? #{:db.type/keyword
+                       :db.type/string
+                       :db.type/boolean
+                       :db.type/long
+                       :db.type/bigint
+                       :db.type/float
+                       :db.type/double
+                       :db.type/bigdec
+                       :db.type/instant
+                       :db.type/uuid
+                       :db.type/uri
+                       :db.type/bytes
+                       :db.type/code}
+                     (:db/ident valueType))))
+      ; [fe] when aggregates or variables
+      (and (not ?dbname) ?last-segment))))
 
 (defn sort-direction [relative-path ctx]
   (let [[sort-path direction] @(::sort-col ctx)]
