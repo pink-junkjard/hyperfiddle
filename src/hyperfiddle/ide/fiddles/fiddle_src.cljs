ad4628ca6e4a7e68dbae503cb2fa46c7865c9b55
Dustin Getz <dustin.getz@gmail.com>
2018-09-23 12:08:29 -0400

    UX: fiddle/ident readonly, with docstring saying to use staging area to edit #644

--- src/hyperfiddle/ide/fiddles/fiddle_src.cljs
+++ src/hyperfiddle/ide/fiddles/fiddle_src.cljs
@@ -67,7 +67,7 @@
           (field [:fiddle/css] ctx hyper-control props))
    :fiddle (fn [val ctx props]
              (fragment
-               (field [:fiddle/ident] ctx hyper-control props)
+               (field [:fiddle/ident] ctx hyper-control (merge props {:read-only true}))
                (field [:fiddle/hydrate-result-as-fiddle] ctx hyper-control props)
                [:div.p "Additional attributes"]
                (->> @(r/fmap ::field/children (:hypercrud.browser/field ctx))
