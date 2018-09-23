ef93da9af35ab3b7663fc2e2578c05a6156fd003
Dustin Getz <dustin.getz@gmail.com>
2018-09-23 12:53:36 -0400

    edn view in schema editor

--- resources/ide/schema_renderer.cljs
+++ resources/ide/schema_renderer.cljs
@@ -1,5 +1,6 @@
 (let [hide-datomic (contrib.reactive/atom true)
       hide-archived (contrib.reactive/atom true)
+      is-edn (contrib.reactive/atom false)
       needle (contrib.reactive/atom nil)
       db-attr? #(<= (:db/id %) 62)
       archived? #(cuerdas.core/starts-with? (namespace (:db/ident %)) "zzz") ; "zzz/" and "zzz.", we are inconsistent
@@ -14,20 +15,24 @@
      [hyperfiddle.ui/markdown (-> ctx :hypercrud.browser/fiddle deref :db/doc)]
      [:div [:label [:input {:type "checkbox" :checked @hide-datomic :on-change #(swap! hide-datomic not)}] " hide Datomic system attributes"]]
      [:div [:label [:input {:type "checkbox" :checked @hide-archived :on-change #(swap! hide-archived not)}] " hide Hyperfiddle archived attributes"]]
+     [:div [:label [:input {:type "checkbox" :checked @is-edn :on-change #(swap! is-edn not)}] " EDN view"]]
      [contrib.ui/text {:value @needle
                        :on-change #(do (reset! needle %))}
       {:placeholder ":task/title"}]
      (let [ctx (-> ctx
                    (update :hypercrud.browser/data (partial contrib.reactive/fmap do-filter-reactive))
                    (assoc :hyperfiddle.ui/layout :hyperfiddle.ui.layout/table))]
-       [hyperfiddle.ui/table
-        (fn [ctx]
-          [(hyperfiddle.ui/field [:db/ident] ctx)
-           (hyperfiddle.ui/field [:db/valueType] ctx #(hyperfiddle.ui.controls/string ((comp (fnil name :–) :db/ident) %) %2 %3) {:read-only true})
-           (hyperfiddle.ui/field [:db/cardinality] ctx #(hyperfiddle.ui.controls/string ((comp (fnil name :–) :db/ident) %) %2 %3) {:read-only true})
-           (hyperfiddle.ui/field [:db/unique] ctx #(hyperfiddle.ui.controls/string ((comp (fnil name :–) :db/ident) %) %2 %3) {:read-only true})
-           (hyperfiddle.ui/field [:db/isComponent] ctx)
-           (hyperfiddle.ui/field [:db/fulltext] ctx nil {:read-only true})
-           (hyperfiddle.ui/field [:db/doc] ctx)])
-        ctx
-        {:hyperfiddle.ui.sort/initial-sort [[:db/ident] :asc]}])]))
+       (if @is-edn
+         [contrib.ui/code {:value (-> @(:hypercrud.browser/data ctx) (->> (map #(dissoc % :db/id))) (js/pprint-str 1000))
+                           :read-only true}]
+         [hyperfiddle.ui/table
+          (fn [ctx]
+            [(hyperfiddle.ui/field [:db/ident] ctx)
+             (hyperfiddle.ui/field [:db/valueType] ctx #(hyperfiddle.ui.controls/string ((comp (fnil name :–) :db/ident) %) %2 %3) {:read-only true})
+             (hyperfiddle.ui/field [:db/cardinality] ctx #(hyperfiddle.ui.controls/string ((comp (fnil name :–) :db/ident) %) %2 %3) {:read-only true})
+             (hyperfiddle.ui/field [:db/unique] ctx #(hyperfiddle.ui.controls/string ((comp (fnil name :–) :db/ident) %) %2 %3) {:read-only true})
+             (hyperfiddle.ui/field [:db/isComponent] ctx)
+             (hyperfiddle.ui/field [:db/fulltext] ctx nil {:read-only true})
+             (hyperfiddle.ui/field [:db/doc] ctx)])
+          ctx
+          {:hyperfiddle.ui.sort/initial-sort [[:db/ident] :asc]}]))]))
