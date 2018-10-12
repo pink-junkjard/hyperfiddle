227337c891b949b71ccbf8880d14070ed516f311
Dustin Getz <dustin.getz@gmail.com>
2018-10-12 16:48:28 -0400

    fiddle src - whack dead code about attr filtering #695

--- src/hyperfiddle/ide/hf_live.cljs
+++ src/hyperfiddle/ide/hf_live.cljs
@@ -16,23 +16,8 @@
 (def attr-order [:fiddle/ident :fiddle/type :fiddle/pull-database :fiddle/pull :fiddle/query
                  :fiddle/renderer :fiddle/css :fiddle/markdown :fiddle/links :fiddle/hydrate-result-as-fiddle])
 
-(defn fiddle-src [attrs initial-tab val ctx-real props]
-  [fiddle-src/fiddle-src-renderer val ctx-real (assoc props :embed-mode true :initial-tab initial-tab)]
-  #_(let [attrs (or (seq attrs)
-                    (clojure.set/intersection
-                      (-> @(:hypercrud.browser/data ctx-real)
-                          keys
-                          (->> (apply sorted-set-by (compare-by-index attr-order)))
-                          (disj :db/id))))
-          ctx (fiddle-src/shadow-fiddle ctx-real)]
-      (fn [val ctx-real props]
-        (into
-          [:div props]
-          (for [k attrs
-                :let [?f (fiddle-src/controls k)
-                      props (when ?f {:embed-mode true})]]
-            ^{:key (str [k])}
-            [field [k] ctx ?f props])))))
+(defn fiddle-src [initial-tab val ctx-real props]
+  [fiddle-src/fiddle-src-renderer val ctx-real (assoc props :embed-mode true :initial-tab initial-tab)])
 
 (defn result-edn [attrs val ctx props]
   (let [s (-> val
@@ -47,8 +32,7 @@
 (defn iframe [ctx props]
   (let [state (r/atom {:edn-fiddle false :edn-result false})]
     (fn [ctx props]
-      (let [fiddle-attrs (::fiddle-attrs props)
-            props (dissoc props ::fiddle-attrs)]
+      (let [props (dissoc props ::fiddle-attrs)]
         [:div.row.hf-live.unp.no-gutters
          (let [as-edn (r/cursor state [:edn-result])
                f (when @as-edn fiddle-api)]
@@ -63,8 +47,8 @@
                                              (assoc :user-renderer f))])])
          (let [as-edn (r/cursor state [:edn-fiddle])
                f (if @as-edn
-                   (r/partial result-edn fiddle-attrs)
-                   (r/partial fiddle-src fiddle-attrs (:initial-tab props)))]
+                   result-edn
+                   (r/partial fiddle-src (:initial-tab props)))]
            [:div.src.col-sm
             [:div "Interactive Hyperfiddle editor:" [contrib.ui/easy-checkbox-boolean " EDN?" as-edn {:class "hf-live"}]]
             [hyperfiddle.ui/iframe ctx {:class (css (:class props) "devsrc hf-live")
