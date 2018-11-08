e1da090e03e7c7dc2e6b0474c24ae9c9819721b9
Karl Hardenstine <khardenstine@gmail.com>
2018-11-08 14:47:02 -0500

    get-secure-db-with+; hydrate-route d/with failures do not fail entire request

--- src/hyperfiddle/service/jvm/hydrate_route.clj
+++ src/hyperfiddle/service/jvm/hydrate_route.clj
@@ -18,7 +18,7 @@
     [taoensso.timbre :as timbre]))
 
 
-(deftype RequestFn [db-with-lookup get-secure-db-with host-env state-atom root-reducer jwt ?subject]
+(deftype RequestFn [db-with-lookup get-secure-db-with+ host-env state-atom root-reducer jwt ?subject]
   runtime/State
   (dispatch! [rt action-or-func] (state/dispatch! state-atom root-reducer action-or-func))
   (state [rt] state-atom)
@@ -32,7 +32,7 @@
     (let [ptm @(runtime/state rt [::runtime/partitions branch :ptm])]
       (-> (if (contains? ptm request)
             (get ptm request)
-            (let [response (hydrate-requests/hydrate-request get-secure-db-with request ?subject)
+            (let [response (hydrate-requests/hydrate-request get-secure-db-with+ request ?subject)
                   ptm (assoc ptm request response)
                   tempid-lookups (hydrate-requests/extract-tempid-lookups db-with-lookup branch)]
               (runtime/dispatch! rt [:hydrate!-success branch ptm tempid-lookups])
@@ -69,9 +69,9 @@
                            (reduce-kv assoc (into {} local-basis)))
           _ (runtime/dispatch! rt [:partition-basis branch local-basis]) ; no one uses this but state should be consistent
           db-with-lookup (atom {})
-          get-secure-db-with (hydrate-requests/build-get-secure-db-with (stage-val->staged-branches stage) db-with-lookup local-basis)
+          get-secure-db-with+ (hydrate-requests/build-get-secure-db-with+ (stage-val->staged-branches stage) db-with-lookup local-basis)
           ; todo should we be sharing state-atom?
-          rt (->RequestFn db-with-lookup get-secure-db-with host-env state-atom root-reducer jwt ?subject)
+          rt (->RequestFn db-with-lookup get-secure-db-with+ host-env state-atom root-reducer jwt ?subject)
           ctx {:branch branch
                ::runtime/branch-aux branch-aux
                :peer rt}
@@ -84,8 +84,7 @@
                  ; must d/with at the beginning otherwise tempid reversal breaks
                  (doseq [[branch-ident branch-content] stage
                          [uri _] branch-content]
-                   ; todo only execute once per uri (for leaf branch, parents are implied)
-                   (get-secure-db-with uri branch-ident)))
+                   (get-secure-db-with+ uri branch-ident)))
       (perf/time (fn [get-total-time] (timbre/debug "Hydrate-route::request-fn" "total time: " (get-total-time)))
                  (foundation/api page-or-leaf ctx (partial ide/api route)))
       (p/resolved (select-keys @(runtime/state rt [::runtime/partitions branch]) [:local-basis :ptm :tempid-lookups]))))
