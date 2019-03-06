ca0a3cab52a39c72133e0f93be2b09a07f3a1580
Karl Hardenstine <khardenstine@gmail.com>
2019-03-06 13:29:00 -0500

    Remove duplicate context/valid check

--- src/hyperfiddle/ui/iframe.cljs
+++ src/hyperfiddle/ui/iframe.cljs
@@ -8,7 +8,6 @@
     [contrib.string :refer [blank->nil]]
     [contrib.ui.safe-render :refer [user-portal]]
     [hypercrud.browser.base :as base]
-    [hypercrud.browser.context :as context]
     [hypercrud.ui.error :as ui-error]
     [hypercrud.ui.stale :as stale]
     [hyperfiddle.runtime :as runtime]))
@@ -51,19 +50,14 @@
                         (update :class css "hyperfiddle-error"))]
     ^{:key (str display-mode)}
     [user-portal (ui-error/error-comp ctx) error-props
-     (either/branch                                         ; Validate context is well-formed and fiddles are valid.
-       (context/valid+ ctx)
-       (fn [e]
-         [:pre (pr-str e #_(:hypercrud.browser/query-validation-issues ctx))])
-       (fn [_]
-         (case display-mode
-           :hypercrud.browser.browser-ui/user (if-let [user-renderer (:user-renderer props)] ; validate qfind and stuff?
-                                                [user-renderer value ctx view-props]
-                                                ; If userland crashes (fiddle/renderer OR cljs-ns), reactions don't take hold, we need to reset here.
-                                                ; Cheaper to pass this as a prop than to hash everything
-                                                [fiddle-renderer-cmp value ctx view-props @(:hypercrud.browser/fiddle ctx)])
-           :hypercrud.browser.browser-ui/xray [hyperfiddle.ui/fiddle-xray value ctx view-props]
-           :hypercrud.browser.browser-ui/api [hyperfiddle.ui/fiddle-api value ctx view-props])))]))
+     (case display-mode
+       :hypercrud.browser.browser-ui/user (if-let [user-renderer (:user-renderer props)] ; validate qfind and stuff?
+                                            [user-renderer value ctx view-props]
+                                            ; If userland crashes (fiddle/renderer OR cljs-ns), reactions don't take hold, we need to reset here.
+                                            ; Cheaper to pass this as a prop than to hash everything
+                                            [fiddle-renderer-cmp value ctx view-props @(:hypercrud.browser/fiddle ctx)])
+       :hypercrud.browser.browser-ui/xray [hyperfiddle.ui/fiddle-xray value ctx view-props]
+       :hypercrud.browser.browser-ui/api [hyperfiddle.ui/fiddle-api value ctx view-props])]))
 
 (defn- fiddle-css-renderer [s] [:style {:dangerouslySetInnerHTML {:__html @s}}])
 
