915d5a2ee6a2a522e26dab541fbf298be23a6eb1
Karl Hardenstine <khardenstine@gmail.com>
2019-03-07 10:41:46 -0500

    Dead code elim

--- resources/ide/console_renderer.cljs
+++
@@ -1,4 +0,0 @@
-(let [{:keys [:hypercrud.browser/fiddle] :as ctx} (hyperfiddle.ui/inject-console-links ctx)]
-  [:div props
-   [hyperfiddle.ui/markdown (:fiddle/markdown @fiddle) ctx]
-   [hyperfiddle.ui/result val ctx {}]])
