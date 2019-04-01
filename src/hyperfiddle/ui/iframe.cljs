3935cd1fc17a548099b8b7e0c11574fd3e946aed
Dustin Getz <dustin.getz@gmail.com>
2019-04-01 13:49:39 -0400

    fiddle/cljs must always run even in xray view #910

--- src/hyperfiddle/ui/iframe.cljs
+++ src/hyperfiddle/ui/iframe.cljs
@@ -25,45 +25,51 @@
 (def ^:private eval-renderer (memoize (fn [code-str & cache-bust] (eval/eval-expr-str!+ code-str))))
 
 (defn- fiddle-renderer-cmp [value ctx props & bust-component-did-update]
-  (let [last-cljs-ns (atom nil)]
+  (let [last-cljs-ns (atom nil)]                            ; don't spam the compiler - memoize buffer of 1
     (fn [value ctx props & bust-component-did-update]
-      (let [cljs-ns @(r/fmap-> (:hypercrud.browser/fiddle ctx) :fiddle/cljs-ns blank->nil)]
-        (when (and (some? cljs-ns) (not= @last-cljs-ns cljs-ns))
-          ; todo maybe use fiddle/ident for ns?
-          (eval-cljs/eval-statement-str! 'user cljs-ns))
+      (let [cljs-ns @(r/fmap-> (:hypercrud.browser/fiddle ctx) :fiddle/cljs-ns blank->nil)] ; always do this
+        (when (and (some? cljs-ns)
+                   (not= @last-cljs-ns cljs-ns))
+          (eval-cljs/eval-statement-str! 'user cljs-ns))    ; Exceptions caught at user-portal error-comp
         (reset! last-cljs-ns cljs-ns)
-        (-> @(r/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/renderer])
-            build-wrapped-render-expr-str
-            (eval-renderer cljs-ns)
-            (either/branch
-              (fn [e] (throw e))
-              (fn [f] [f value ctx props])))))))
+
+        (let [display-mode (or (some-> (:hypercrud.ui/display-mode ctx) deref)
+                               :hypercrud.browser.browser-ui/user)
+              props props #_(select-keys props [:class :initial-tab :on-click #_:disabled])]
+          (case display-mode
+
+            :hypercrud.browser.browser-ui/user
+            (if-let [user-renderer (:user-renderer props)]  ; validate qfind and stuff?
+              [user-renderer value ctx props]
+              (-> @(r/cursor (:hypercrud.browser/fiddle ctx) [:fiddle/renderer])
+                  build-wrapped-render-expr-str
+                  (eval-renderer cljs-ns)
+                  (either/branch
+                    (fn [e] (throw e))
+                    (fn [f] [f value ctx props]))))
+
+            :hypercrud.browser.browser-ui/xray
+            (if-let [user-renderer (:user-renderer props)]
+              ; Select user-renderer is valid in xray mode now
+              [user-renderer value ctx props]
+              [hyperfiddle.ui/fiddle-xray value ctx props])
+
+            :hypercrud.browser.browser-ui/api
+            [hyperfiddle.ui/fiddle-api value ctx props]))))))
 
 (defn- ui-comp [ctx & [props]]                              ; user-renderer comes through here
   (let [value @(:hypercrud.browser/result ctx)              ; TODO remove this, make them ask
         props (update props :class css (auto-ui-css-class ctx))
         ; The reactdom component should filter the keys at the last second.
         ; https://github.com/hyperfiddle/hyperfiddle/issues/698
-        view-props props #_(select-keys props [:class :initial-tab :on-click #_:disabled])
         display-mode (or (some-> (:hypercrud.ui/display-mode ctx) deref) :hypercrud.browser.browser-ui/user)
         error-props (-> (select-keys props [:class :on-click])
                         (update :class css "hyperfiddle-error"))]
     ^{:key (str display-mode)}
     [user-portal (ui-error/error-comp ctx) error-props
-     (case display-mode
-       :hypercrud.browser.browser-ui/user (if-let [user-renderer (:user-renderer props)] ; validate qfind and stuff?
-                                            [user-renderer value ctx view-props]
-                                            ; If userland crashes (fiddle/renderer OR cljs-ns), reactions don't take hold, we need to reset here.
-                                            ; Cheaper to pass this as a prop than to hash everything
-                                            [fiddle-renderer-cmp value ctx view-props @(:hypercrud.browser/fiddle ctx)])
-
-       ; TODO cljs-ns is valid in xray mode (for defmethod)
-       :hypercrud.browser.browser-ui/xray (if-let [user-renderer (:user-renderer props)]
-                                            ; Select user-renderer is valid in xray mode now
-                                            [user-renderer value ctx view-props]
-                                            [hyperfiddle.ui/fiddle-xray value ctx view-props])
-
-       :hypercrud.browser.browser-ui/api [hyperfiddle.ui/fiddle-api value ctx view-props])]))
+     ; If userland crashes (fiddle/renderer OR cljs-ns), reactions don't take hold, we need to reset here.
+     ; Cheaper to pass fiddle-value as a prop than to hash everything
+     [fiddle-renderer-cmp value ctx props @(:hypercrud.browser/fiddle ctx)]]))
 
 (defn- fiddle-css-renderer [s] [:style {:dangerouslySetInnerHTML {:__html @s}}])
 
